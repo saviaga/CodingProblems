715c860908609e161c54f772604d39a62d03475c
saviaga <saviaga@gmail.com>
2022-06-19 09:17:16 -0400

    Time: 495 ms (95.72%), Space: 0B (100.00%) - LeetHub

---
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -0,0 +1,9 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+
+
+DELETE FROM Person WHERE id NOT IN 
+(SELECT * FROM 
+(SELECT min(id) FROM Person 
+GROUP BY email) as sq)
+
