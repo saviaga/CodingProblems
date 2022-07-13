160757f7ca61d86b783c2478fdd73b0bfba8c92c
saviaga <saviaga@gmail.com>
2022-07-13 09:21:49 -0400

    Time: 1180 ms (11.24%), Space: 25.7 MB (46.28%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,15 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        curr = sum_max = nums[0]
         
-        if len(nums)==0:
-            return 0
-        if len(nums)==1:
-            return nums[0]
+        max_sum = nums[0]
+        together = nums[0]
         
-        for i in range(1,len(nums)):
-            curr = max(nums[i],nums[i]+curr)
-            sum_max = max(sum_max,curr)
-        return sum_max
+        for i in range(1,len(nums)):           
+            together += nums[i]
+            if together < nums[i]:
+                together = nums[i]
+            max_sum = max(max_sum,together)
+                
+        return max_sum
+            
         
