4aebf89a62ef42232b54ac69240ed357e94fa090
saviaga <saviaga@gmail.com>
2022-07-16 08:35:08 -0400

    Time: 48 ms (65.10%), Space: 13.6 MB (33.38%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -13,21 +13,13 @@ class Solution(object):
             return self.intersect(nums2,nums1)
         else:
             res = []
-            p1 = 0
-            p2 = 0
-            nums1.sort()
-            nums2.sort()
-            while p1<len(nums1) and p2 < len(nums2):
-                
-                if nums1[p1] < nums2[p2]:
-                    p1+=1
-                elif nums1[p1] > nums2[p2]:
-                    p2+=1
-                elif nums1[p1] == nums2[p2]:
-                   
-                    res.append(nums1[p1])
-                    p2+=1
-                    p1+=1
+            dic_nums1 = Counter(nums1)
+            for elem in nums2:
+                if elem in dic_nums1:
+                    dic_nums1[elem] -=1
+                    res.append(elem)
+                    if dic_nums1[elem]==0:
+                        del dic_nums1[elem]
             return res
 
                 
