b2a657972401fd937b74c14005f79cd049c5e322
saviaga <saviaga@gmail.com>
2022-07-19 18:08:10 -0400

    Time: 28 ms (53.74%), Space: 13.7 MB (12.78%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        bracket_map = {")": "(", "]": '[',  "}": "{"}
+        
+        stack = []
+        for elem in s:
+                if elem in bracket_map:
+                    if stack:
+                        top = stack.pop()
+                    else:
+                        top = '#'
+                    if bracket_map[elem]!=top:
+                        return False
+                else:
+                    stack.append(elem)
+                    
+         # In the end, if the stack is empty, then we have a valid expression.
+        return stack == []
+    #Time complexity : O(n) because we simply traverse the given string one character at a time and push and pop operations on a stack take O(1) time.
+    #Space complexity : O(n) as we push all opening brackets onto the stack and in the worst case, we will end up pushing all the brackets onto the stack. e.g. ((((((((((
+            
+                
+                
