a0c18243d1ad44362edfea3f8c3e323e7a64cd5c
saviaga <saviaga@gmail.com>
2022-07-20 11:28:34 -0400

    Time: 35 ms (29.72%), Space: 13.4 MB (81.46%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -4,22 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        bracket_map = {")": "(", "]": '[',  "}": "{"}
+        dic_map = {')':'(',']':'[','}':'{'}
         
         stack = []
         for elem in s:
-                if elem in bracket_map:
-                    if stack and stack[-1] == bracket_map[elem] :
-                        stack.pop()
-                    else:
-                        return False
-                else:
-                    stack.append(elem)
-                    
-         # In the end, if the stack is empty, then we have a valid expression.
-        return stack == []
-    #Time complexity : O(n) because we simply traverse the given string one character at a time and push and pop operations on a stack take O(1) time.
-    #Space complexity : O(n) as we push all opening brackets onto the stack and in the worst case, we will end up pushing all the brackets onto the stack. e.g. ((((((((((
             
+            if elem in dic_map:
+                if stack and stack[-1] == dic_map[elem]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(elem)
+        return stack ==[]
+    
+    
                 
-                
+        
