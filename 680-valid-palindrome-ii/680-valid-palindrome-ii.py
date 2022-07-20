d68085dc11ae1175488693974306a438da9f968f
saviaga <saviaga@gmail.com>
2022-07-20 11:46:55 -0400

    Time: 231 ms (24.80%), Space: 14.2 MB (58.73%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -4,28 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        
-        def check_removing_one(s, i,j):
-            while i <j:
-                if s[i]!=s[j]:
+        def check_removing_one(s,i,j):
+            
+            while i <=j:
+                if s[i] == s[j]:
+                    i+=1
+                    j-=1
+                else:
                     return False
-                i+=1
-                j-=1
             return True
         
-    
+        
         start = 0
         end = len(s)-1
-        while start <end:
-            if s[start] != s[end]:
-                
-                return check_removing_one(s,start+1,end) or  check_removing_one(s,start,end-1)
-            start+=1
-            end-=1
-
+        
+        while start <=end:
+            if s[start] == s[end]:
+                start+=1
+                end-=1
+               
+            else:
+                return check_removing_one(s,start+1,end) or check_removing_one(s,start,end-1)
         return True
             
-            
-            
-        
-      
