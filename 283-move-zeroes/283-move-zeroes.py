7463f126ccea8f224dc4abbb83293e6e515b9535
saviaga <saviaga@gmail.com>
2022-07-20 17:34:33 -0400

    Time: 197 ms (62.51%), Space: 14.8 MB (33.60%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,24 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        n = 0
+        z = 0
         
-        idx0 = 0
-        idxn = 0
-        
-        while idxn < len(nums):
-            if nums[idx0]==0 and nums[idxn]!=0:
-                nums[idx0], nums[idxn] = nums[idxn],nums[idx0]
-                idx0+=1
-                idxn+=1
-            
-            elif nums[idx0]==0 and nums[idxn]==0:
-                idxn+=1
-            else:
-                idx0+=1
-                idxn+=1
-                
-                
+        while n < len(nums):
+            if nums[z] == 0 and nums[n] == 0:
+                n+=1
+            elif nums[z] == 0 and nums[n] !=0:
+                nums[z],nums[n] = nums[n],nums[z]
+                n+=1
+                z+=1
+            elif nums[z] !=0 and nums[n] !=0:
+                n+=1
+                z+=1
         return nums
-            
-            
+        
+        
         
