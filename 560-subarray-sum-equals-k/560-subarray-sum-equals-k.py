d11c51dc3d65f3543c8b70b2dbc0d3931e0d7685
saviaga <saviaga@gmail.com>
2022-07-20 12:45:18 -0400

    Time: 641 ms (5.05%), Space: 16 MB (67.78%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        count = 0
+        cs = 0
+        dict_sum = {0:1}
+        
+        for i in range(len(nums)):           
+            cs += nums[i];
+            
+            if (cs - k) in dict_sum: 
+                count += dict_sum[cs - k]
+            
+            if cs in  dict_sum:
+                dict_sum[cs]+=1
+            else:
+                dict_sum[cs] = 1
+
+               
+        print(dict_sum)
+        return count
+                
+                
+            
+            
+        
