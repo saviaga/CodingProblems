bd12f0ebda934deaf6be2ae1cc93ef694eb35bca
saviaga <saviaga@gmail.com>
2022-07-22 14:01:32 -0400

    Time: 52 ms (96.40%), Space: 21.2 MB (93.67%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -49,3 +49,8 @@ class Solution(object):
             
         return q
         
+#Complexity Analysis
+
+#Time Complexity: O(N), where NN is the number of nodes in the binary tree. In the worst case we might be visiting all the nodes of the binary tree.
+
+#Space Complexity: O(N). This is because the maximum amount of space utilized by the recursion stack would be NN since the height of a skewed binary tree could be NN.        
