12e27d4f4349a598e6de61e0a192a73cea824e1f
saviaga <saviaga@gmail.com>
2022-07-24 15:43:27 -0400

    Time: 36 ms (93.25%), Space: 13.5 MB (83.67%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -5,22 +5,21 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        res = []
-        stack = []
         dic_pairs = {}
-        
-        for i in range(len(nums2)):
-            while stack and nums2[i] > stack[-1]:
-                dic_pairs[stack.pop()] = nums2[i]
-            stack.append(nums2[i])
-        
+        stack = []
+        res = []
+        for elem in nums2:
+            while stack and elem > stack[-1]:
+                dic_pairs[stack.pop()]=elem 
+            stack.append(elem)
+            
         while stack:
-            dic_pairs[stack.pop()] = -1
-        print(dic_pairs)
-        for i in range(len(nums1)):
-            res.append(dic_pairs[nums1[i]])
+            dic_pairs[stack.pop()]=-1
+        
+        for elem in nums1:
+            res.append(dic_pairs[elem])
             
         return res
-        
-                    
-                    
+    
+    
+    
