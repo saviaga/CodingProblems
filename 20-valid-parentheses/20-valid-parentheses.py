46bbd265ed1346f18bf94a593d1c5af45d1cdc4a
saviaga <saviaga@gmail.com>
2022-07-25 19:49:29 -0400

    Time: 47 ms (54.09%), Space: 13.8 MB (97.84%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,23 +1,17 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        dic_map = {')':'(',']':'[','}':'{'}
+class Solution:
+    def isValid(self, s: str) -> bool:
         
+        dic_par = {')':'(',']':'[','}':'{'}
         stack = []
         for elem in s:
-            
-            if elem in dic_map:
-                if stack and stack[-1] == dic_map[elem]:
+            if elem == '(' or elem == '[' or elem == '{':
+                stack.append(elem)
+            elif elem in dic_par:
+                if stack and stack[-1] == dic_par[elem]:
                     stack.pop()
                 else:
                     return False
-            else:
-                stack.append(elem)
-        return stack ==[]
-    
-    
+
+        return stack==[]
                 
-        
+                
