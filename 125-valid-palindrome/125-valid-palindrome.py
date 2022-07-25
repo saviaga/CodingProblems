53800ae443e089b86de187c380b6265262d8d3a6
saviaga <saviaga@gmail.com>
2022-07-25 19:35:52 -0400

    Time: 76 ms (48.39%), Space: 14.4 MB (57.28%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,25 +1,26 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
         
         start = 0
         end = len(s)-1
         
-        while start < end:
-            
-            if not s[start].isalnum():                
+        while start< end:
+
+            if not s[start].isalnum():
                 start+=1
                 continue
-            if not s[end].isalnum():               
+            if not s[end].isalnum():
                 end-=1
                 continue
+                
             if s[start].lower()!=s[end].lower():
                 return False
             start+=1
             end-=1
         return True
+        
+    
+
                 
         
