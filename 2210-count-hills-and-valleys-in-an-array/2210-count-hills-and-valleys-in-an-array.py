e0cc29b7e58a2cbf3649d07b9622fd72a3ab8154
saviaga <saviaga@gmail.com>
2022-07-26 15:22:01 -0400

    Time: 69 ms (30.81%), Space: 13.8 MB (69.32%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -1,24 +1,22 @@
-class Solution(object):
-    def countHillValley(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        
         #remove duplicates
-        result=0
+        result =0
         no_dup = [nums[0]]
         for i in range(1,len(nums)):
             if nums[i]!=nums[i-1]:
                 no_dup.append(nums[i])
+     
         
-                
         for i in range(1,len(no_dup)-1):
-            #count hill
-            if no_dup[i]> no_dup[i-1] and no_dup[i]>no_dup[i+1]:
+            #hill
+            if no_dup[i]>no_dup[i-1] and no_dup[i]>no_dup[i+1]:
                 result+=1
-            if no_dup[i] < no_dup[i-1] and no_dup[i] < no_dup[i+1]:
-                 result+=1
-            
+            #valley
+            elif no_dup[i]<no_dup[i-1] and no_dup[i]<no_dup[i+1]:
+                result+=1
         return result
-            
-            
+        
+     
+       
