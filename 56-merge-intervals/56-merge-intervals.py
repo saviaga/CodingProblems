1a82f55699aa0a38a510c90279effd2119d77c5c
saviaga <saviaga@gmail.com>
2022-07-26 09:13:40 -0400

    Time: 338 ms (6.06%), Space: 18.1 MB (51.40%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res =[]
+        intervals.sort(reverse=True)
+        print(intervals)
+        while intervals:
+            current = intervals.pop()
+            
+            start = current[0]
+            end = current[1]
+            
+            if intervals and (intervals[-1][0] >=start and intervals[-1][0]<= end):
+                next_i = intervals.pop()
+                intervals.append([start,max(end,next_i[1])])
+
+            else:
+                res.append(current)
+        return res
+                
+                
