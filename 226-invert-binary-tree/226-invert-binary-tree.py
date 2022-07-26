9a77aac80139c96c4dc4f62fe83a5eb81a610f69
saviaga <saviaga@gmail.com>
2022-07-26 09:32:04 -0400

    Time: 45 ms (58.39%), Space: 13.9 MB (56.44%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -1,34 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def invertTree(self, root):
-    
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-        #base case
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
         if root == None:
-            return
+            return None
         
-        #invert
-        right =self.invertTree(root.right)
+        right = self.invertTree(root.right)
         left = self.invertTree(root.left)
-        
-        #swap children
         root.left = right
         root.right = left
-        
         return root
-        #iterative
-        #Since each node in the tree is visited / added to the queue only once, the time complexity is O(n), where nn is the number of nodes in the tree.
-
-#Space complexity is O(n), since in the worst case, the queue will contain all nodes in one level of the binary tree. For a full binary tree, the leaf level has n/2 = 0(n) leaves.
-
-        #recursion
-    # space is the number of recursive calls 
-    #Because of recursion, O(h) function calls will be placed on the stack in the worst case, where h is the height of the tree. Because h∈O(n), the space complexity is O(n).
+        
+        
+        
