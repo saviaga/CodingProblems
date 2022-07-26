75aa5db088d03f7a6ce46e0b5d7a00ed9309e7d5
saviaga <saviaga@gmail.com>
2022-07-26 11:39:41 -0400

    Time: 59 ms (34.81%), Space: 13.9 MB (33.26%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -1,41 +1,37 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def verticalOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
         if root == None:
             return
-        max_col = 0
+        
+        
+        queue=collections.deque([[root,0]])
+        res = defaultdict(list)
         min_col = 0
-        
-        queue = collections.deque([(root,0)])
-        all_levels = defaultdict(list)
-        res = []
-        
+        max_col = 0
+        all_levels = []
         while queue:
-            
             for _ in range(len(queue)):
                 curr,col = queue.popleft()
-                max_col = max(max_col,col)
                 min_col = min(min_col,col)
-                all_levels[col].append(curr.val)
+                max_col = max(max_col,col)
+                res[col].append(curr.val)
                 if curr.left:
-                    queue.append((curr.left,col-1))
+                    queue.append([curr.left,col-1])
                 if curr.right:
-                    queue.append((curr.right,col+1))
+                    queue.append([curr.right,col+1])
+        
         
         for i in range(min_col,max_col+1):
-            res.append(all_levels[i])
-        return res
-                                 
-                
-                                 
-                
+            all_levels.append(res[i])
+            
+        return all_levels
+        
+        
         
