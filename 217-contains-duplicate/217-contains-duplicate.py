40f94fa5c0d18b41d3f36ad228b74b659daf1019
saviaga <saviaga@gmail.com>
2022-07-27 17:37:30 -0400

    Time: 633 ms (55.86%), Space: 25.9 MB (72.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,10 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        dict_count = {}
+        dic_count = {}
         for elem in nums:
-            if elem not in dict_count:
-                dict_count[elem] = 1
-            else:
+            if elem in dic_count:
                 return True
-        
+            else:
+                dic_count[elem] = 1
