e68245b5c07d34fbc0dffd447be479e455d3bb51
saviaga <saviaga@gmail.com>
2022-07-27 09:19:36 -0400

    Time: 86 ms (87.99%), Space: 18.7 MB (96.95%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,35 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
         
-        p_val = p.val
-        q_val = q.val
-        
-        
-        
+       
         while root:
-            parent_val = root.val
-        #If both p and q are greater than parent, then search in the left subtree
-            if p_val > parent_val and q_val > parent_val:
+            parent = root.val
+            if p.val > parent and q.val > parent:
                 root = root.right
-        #If both p and q are lesser than parent, then search in the right subtree
-            elif p_val < parent_val and q_val < parent_val:
+                
+            elif p.val < parent and q.val < parent:
                 root = root.left
-        
-        # if p is in one side and q is in the other then the root is the lca
+                
             else:
                 return root
         
