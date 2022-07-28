6b68ee69e97d2f13aaf30027255b80d9ea50847f
saviaga <saviaga@gmail.com>
2022-07-28 12:22:08 -0400

    Time: 63 ms (24.31%), Space: 17 MB (6.43%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -5,40 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    
-    
-    def height(self,root):
-        if not root:
-            return 0
-        stack = [(0,root)]
-        max_height = 0
-        while stack:
-            current_height, current_node = stack.pop()
-            max_height = max(max_height,current_height)
-            if current_node:
-                stack.append((current_height+1,current_node.left))
-                stack.append((current_height+1,current_node.right))
-        return max_height
-    
     def diameterOfBinaryTree(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if root == None:
-            return
         
+        stack = [(root,False)]
+        diameter = 0
+        heights = {}
         
-        stack = [(root,0)]
-        max_diam = 0
-        max_height = 0
         while stack:
-            cur_node, curr_diam = stack.pop()
-            if cur_node.right:
-                stack.append((cur_node.right,curr_diam+1))
-            if cur_node.left:
-                stack.append((cur_node.left,curr_diam+1))
+            curr,visited = stack.pop()
             
-            cur_height = self.height(cur_node.left)+self.height(cur_node.right)
-            max_diam = max(max_diam,max(cur_height ,curr_diam))               
-        return max_diam
+            if visited:
+                hleft = heights.get(curr.left, 0)
+                hright = heights.get(curr.right,0)
+                
+                diameter = max(diameter,hleft+hright)
+                
+                heights[curr] = max(hleft,hright)+1
+            else:
+                stack.append((curr,True))
+                if curr.right:
+                     stack.append((curr.right,False))
+                if curr.left:
+                     stack.append((curr.left,False))
+        return diameter
