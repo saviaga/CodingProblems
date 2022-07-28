b8b3c9b8971a258af685d0dccc5e136f2c593b2d
saviaga <saviaga@gmail.com>
2022-07-28 12:05:52 -0400

    Time: 880 ms (5.93%), Space: 16.3 MB (42.04%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -5,32 +5,40 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    
+    
+    def height(self,root):
+        if not root:
+            return 0
+        stack = [(0,root)]
+        max_height = 0
+        while stack:
+            current_height, current_node = stack.pop()
+            max_height = max(max_height,current_height)
+            if current_node:
+                stack.append((current_height+1,current_node.left))
+                stack.append((current_height+1,current_node.right))
+        return max_height
+    
     def diameterOfBinaryTree(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        
-        def diameterOfBinaryTreeHelper(root):
-            if root == None:
-                return 0
-            
-            left = diameterOfBinaryTreeHelper(root.left)
-            right = diameterOfBinaryTreeHelper(root.right)
-            
-            self.diameter = max(self.diameter, left + right )
-            
-            return max(left,right) + 1
-            
-            
-            
-            
-            
-            
-            
-            
-        self.diameter = 0
-        diameterOfBinaryTreeHelper(root)
-        return self.diameter
+        if root == None:
+            return
         
         
+        stack = [(root,0)]
+        max_diam = 0
+        max_height = 0
+        while stack:
+            cur_node, curr_diam = stack.pop()
+            if cur_node.right:
+                stack.append((cur_node.right,curr_diam+1))
+            if cur_node.left:
+                stack.append((cur_node.left,curr_diam+1))
+            
+            cur_height = self.height(cur_node.left)+self.height(cur_node.right)
+            max_diam = max(max_diam,max(cur_height ,curr_diam))               
+        return max_diam
