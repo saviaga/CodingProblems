a8e5e2bc57719d61585620b05d5572bf6d752717
saviaga <saviaga@gmail.com>
2022-07-28 11:53:28 -0400

    Time: 51 ms (47.26%), Space: 16.2 MB (66.35%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -10,22 +10,27 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        
         def diameterOfBinaryTreeHelper(root):
             if root == None:
                 return 0
             
-
-            left =  diameterOfBinaryTreeHelper(root.left)
+            left = diameterOfBinaryTreeHelper(root.left)
             right = diameterOfBinaryTreeHelper(root.right)
             
-            self.diameter = max(self.diameter,left + right)
+            self.diameter = max(self.diameter, left + right )
             
-            return max(left,right) + 1    
-    
+            return max(left,right) + 1
+            
+            
+            
+            
+            
+            
+            
+            
         self.diameter = 0
         diameterOfBinaryTreeHelper(root)
         return self.diameter
         
-        #Time complexity: O(N). This is because in our recursion function longestPath, we only enter and exit from each node once. We know this because each node is entered from its parent, and in a tree, nodes only have one parent.
-
-#Space complexity: O(N). The space complexity depends on the size of our implicit call stack during our DFS, which relates to the height of the tree. In the worst case, the tree is skewed so the height of the tree is O(N)O(N). If the tree is balanced, it'd be O(\log N)O(logN).
+        
