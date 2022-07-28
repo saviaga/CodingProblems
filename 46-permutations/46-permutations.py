273f51df1e91e156d39100cefcb7ec62dba1c8ae
saviaga <saviaga@gmail.com>
2022-07-28 17:24:28 -0400

    Time: 42 ms (42.01%), Space: 13.6 MB (64.09%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -7,8 +7,8 @@ class Solution(object):
         
         def permuteHelper(nums,chosen):
             if len(nums)==0:
-               
-                return res.append(chosen[:])
+                print(chosen)
+                res.append(chosen[:])
             else:
                 
                 for i in range(len(nums)):  
@@ -22,17 +22,7 @@ class Solution(object):
                     #unchose
                     nums.insert(i,element)
                     chosen.pop()
-        
-      
         res = []            
         permuteHelper(nums,[])
         return res
         
-        
-                
-           
-            
-                    
-                   
-                
-                
