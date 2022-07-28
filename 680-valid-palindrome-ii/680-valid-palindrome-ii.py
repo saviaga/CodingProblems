435d2468513f36835f604d8a43438ee847ba29d2
saviaga <saviaga@gmail.com>
2022-07-28 15:06:09 -0400

    Time: 198 ms (36.08%), Space: 14 MB (90.78%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,29 +1,36 @@
 class Solution(object):
+    def validatepalindrome(self,s,start,end):
+
+            while start<=end:
+                if s[start]==s[end]:
+                    start+=1
+                    end-=1
+                else:
+                    return False
+            return True
     def validPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        def check_removing_one(s,i,j):
-            
-            while i <=j:
-                if s[i] == s[j]:
-                    i+=1
-                    j-=1
-                else:
-                    return False
-            return True
         
-        
+    
+    
         start = 0
         end = len(s)-1
         
-        while start <=end:
-            if s[start] == s[end]:
+        while start<=end:
+            
+            if s[start]==s[end]:
                 start+=1
                 end-=1
-               
             else:
-                return check_removing_one(s,start+1,end) or check_removing_one(s,start,end-1)
+                #look withouth elem in right
+                left = self.validatepalindrome(s,start+1,end)
+                right = self.validatepalindrome(s,start,end-1)
+                #look without elem in left
+                return left or right
+                
         return True
+            
             
