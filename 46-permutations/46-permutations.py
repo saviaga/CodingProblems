5cea4a40a5127075662de6637d3b017683676d16
saviaga <saviaga@gmail.com>
2022-07-28 16:45:52 -0400

    Time: 40 ms (47.90%), Space: 13.6 MB (64.09%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        def permuteHelper(nums,chosen):
+            if len(nums)==0:
+                print(chosen)
+                return res.append(chosen[:])
+            else:
+                
+                for i in range(len(nums)):  
+                    
+                    #choose
+                    element = nums[i]
+                    chosen.append(element)
+                    nums.remove(element)
+                    #explore
+                    permuteHelper(nums,chosen)
+                    #unchose
+                    nums.insert(i,element)
+                    chosen.pop()
+        res = []            
+        permuteHelper(nums,[])
+        return res
+        
+        
+                
+           
+            
+                    
+                   
+                
+                
