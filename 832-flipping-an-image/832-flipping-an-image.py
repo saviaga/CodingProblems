57d1621aeaf6c0647d9e40fc950c306b3cd61577
saviaga <saviaga@gmail.com>
2022-07-29 10:48:42 -0400

    Time: 61 ms (31.63%), Space: 13.3 MB (91.55%) - LeetHub

---
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        """
+        :type image: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
+        for row in image:
+            i, j = 0, len(row) - 1
+            while i <= j:
+                if row[i] == row[j]:
+                    row[i], row[j] = row[i]^1, row[j]^1
+                i += 1
+                j -= 1
+        return image
