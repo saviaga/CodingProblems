ff0f2e6bbd30f3eaad470eb24cb7e86e556d8c15
saviaga <saviaga@gmail.com>
2022-07-29 11:39:38 -0400

    Time: 146 ms (50.94%), Space: 14.6 MB (53.07%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -5,17 +5,19 @@ class Solution(object):
         :rtype: int
         """
         
-        res = 0
+      
 
         row = len(grid)-1
-        col = 0
-        count = 0
+        col,count = 0,0
+       
         while row>=0 and col< len(grid[0]):
             if grid[row][col] < 0:
                 count +=len(grid[0])-col
                 row -= 1  #if negative found move to the upper row
             else:
                 col +=1  #if negative not found keep looking in next column
-        return(count)
+        return count
     
+        #Time complexity O(N)
+        #Space complexity O(1)
     
