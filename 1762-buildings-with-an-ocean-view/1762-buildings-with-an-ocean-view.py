c0475576d4d0b9ce2ee11ee31aa0fd6cc7a631d3
saviaga <saviaga@gmail.com>
2022-07-28 21:50:43 -0400

    Time: 546 ms (99.00%), Space: 29.6 MB (26.60%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -4,19 +4,15 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[int]
         """
-        max_height = heights[-1]
         
-        i = len(heights)-1
-        view = collections.deque([i])
-        while i>0:
-            
-            if heights[i] < heights[i-1] and heights[i-1] > max_height:
-                max_height = max(max_height,heights[i-1])
-                view.appendleft(i-1)
-            i-=1
-        if heights[0]>max_height:
-            view.appendleft(i-1)
-        return view
+
+        right_max = 0
+        res = deque()
+        for i in range(len(heights)-1, -1, -1):
+            if right_max < heights[i]:
+                res.appendleft(i)
+                right_max = heights[i]
+        return res
             
                 
         
