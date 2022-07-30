3d18028663bb2a2faff0dfa76f789748d40ff7fd
saviaga <saviaga@gmail.com>
2022-07-30 11:55:04 -0400

    Time: 23 ms (68.76%), Space: 13.3 MB (70.39%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -8,33 +8,31 @@ class Solution(object):
         
         top = 0
         left = 0
-        bottom = len(mat)-1 
-        right = len(mat[0])-1
+        bottom = len(mat)
+        right = len(mat[0])
         
         
-        while (top <= bottom and left <= right):
+        while (top < bottom and left < right):
             
             #left to right
-            for col in range(left,right+1):
+            for col in range(left,right):
                 res.append(mat[top][col])  #add top row 
             top += 1
             
             #top to bottom
-            for row in range(top,bottom+1):
-                res.append(mat[row][right]) # add right column
+            for row in range(top,bottom):
+                res.append(mat[row][right-1]) # add right column
             right -= 1
             
             #right to left
-            if (top <= bottom): #to avoid repeating right to left if only 1 col mat
-               
-                for col in reversed(range(left,right+1)):
-                    res.append(mat[bottom][col]) #add bottom row 
+            if (top < bottom): #to avoid repeating right to left if only 1 col mat
+                for col in reversed(range(left,right)):
+                    res.append(mat[bottom-1][col]) #add bottom row 
                 bottom -= 1
                 
             #bottom to top   
-            if (left <= right): #to avoid repeating bottom to top if only 1 row mat
-
-                for row in reversed(range(top,bottom+1)):
+            if (left < right): #to avoid repeating bottom to top if only 1 row mat
+                for row in reversed(range(top,bottom)):
                     res.append(mat[row][left]) #add bottom column
                 left += 1
         return res
