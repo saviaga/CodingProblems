9f580e0783530cd4938c2b77a96a4a1428abc1c7
saviaga <saviaga@gmail.com>
2022-07-30 11:34:34 -0400

    Time: 19 ms (84.59%), Space: 13.3 MB (70.39%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -15,25 +15,25 @@ class Solution(object):
         while (top <= bottom and left <= right):
             
             #left to right
-            for i in range(left,right+1):
-                res.append(mat[top][i])  #add top row 
+            for col in range(left,right+1):
+                res.append(mat[top][col])  #add top row 
             top += 1
             
             #top to bottom
-            for i in range(top,bottom+1):
-                res.append(mat[i][right]) # add right column
+            for row in range(top,bottom+1):
+                res.append(mat[row][right]) # add right column
             right -= 1
             
-            #left to right
-            if (top <= bottom): #to avoid repeating left to right if only 1 col mat
-                for i in reversed(range(left,right+1)):
-                    res.append(mat[bottom][i]) #add bottom row 
+            #right to left
+            if (top <= bottom): #to avoid repeating right to left if only 1 col mat
+                for col in range(right,left-1,-1):
+                    res.append(mat[bottom][col]) #add bottom row 
                 bottom -= 1
                 
             #bottom to top   
             if (left <= right): #to avoid repeating bottom to top if only 1 row mat
-                for i in reversed(range(top,bottom+1)):
-                    res.append(mat[i][left]) #add bottom column
+                for row in reversed(range(top,bottom+1)):
+                    res.append(mat[row][left]) #add bottom column
                 left += 1
         return res
             
