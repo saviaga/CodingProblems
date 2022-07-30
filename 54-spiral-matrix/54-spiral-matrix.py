50d6eb750c323d8b21ea9eeb66a2a3c54607f4a7
saviaga <saviaga@gmail.com>
2022-07-30 11:29:12 -0400

    Time: 38 ms (11.80%), Space: 13.5 MB (44.72%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -6,35 +6,35 @@ class Solution(object):
         """
         res = []
         
-        row_begin = 0
-        col_begin = 0
-        row_end = len(mat)-1 
-        col_end = len(mat[0])-1
+        top = 0
+        left = 0
+        bottom = len(mat)-1 
+        right = len(mat[0])-1
         
         
-        while (row_begin <= row_end and col_begin <= col_end):
+        while (top <= bottom and left <= right):
             
             #left to right
-            for i in range(col_begin,col_end+1):
-                res.append(mat[row_begin][i])  #add top row 
-            row_begin += 1
+            for i in range(left,right+1):
+                res.append(mat[top][i])  #add top row 
+            top += 1
             
             #top to bottom
-            for i in range(row_begin,row_end+1):
-                res.append(mat[i][col_end]) # add right column
-            col_end -= 1
+            for i in range(top,bottom+1):
+                res.append(mat[i][right]) # add right column
+            right -= 1
             
             #left to right
-            if (row_begin <= row_end): #to avoid repeating left to right if only 1 col mat
-                for i in range(col_end,col_begin-1,-1):
-                    res.append(mat[row_end][i]) #add bottom row 
-                row_end -= 1
+            if (top <= bottom): #to avoid repeating left to right if only 1 col mat
+                for i in range(right,left-1,-1):
+                    res.append(mat[bottom][i]) #add bottom row 
+                bottom -= 1
                 
             #bottom to top   
-            if (col_begin <= col_end): #to avoid repeating bottom to top if only 1 row mat
-                for i in range(row_end,row_begin-1,-1):
-                    res.append(mat[i][col_begin]) #add bottom column
-                col_begin += 1
+            if (left <= right): #to avoid repeating bottom to top if only 1 row mat
+                for i in range(bottom,top-1,-1):
+                    res.append(mat[i][left]) #add bottom column
+                left += 1
         return res
             
             
