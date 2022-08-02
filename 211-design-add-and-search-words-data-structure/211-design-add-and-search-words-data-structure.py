dd97626a16a26222a40ea1fd053ee445b7318def
saviaga <saviaga@gmail.com>
2022-08-02 15:32:02 -0400

    Time: 13531 ms (69.36%), Space: 119.7 MB (19.27%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -1,33 +1,38 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.end_node = 0
-        
-class WordDictionary:
-    def __init__(self):
-        self.root = TrieNode()      
+        self.isEndNode = False
 
-    def addWord(self, word):
-        root = self.root
-        for symbol in word:
-            root = root.children.setdefault(symbol, TrieNode())
-        root.end_node = 1
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
         
+    def addWord(self, word):
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
+        cur.isEndNode = True     
+
     def search(self, word):
-        def dfs(node, i):
-            if i == len(word): return node.end_node
-               
-            if word[i] == ".":
-                for child in node.children:
-                    if dfs(node.children[child], i+1): return True
-                    
-            if word[i] in node.children:
-                return dfs(node.children[word[i]], i+1)
-            
-            return False
-    
-        return dfs(self.root, 0)
-    
+        def dfs(j,child):
+            cur = child
+            for i in range(j,len(word)):
+                w = word[i]
+                if w==".":
+                    for child in cur.children.values():
+                        if dfs(i+1,child):
+                            return True
+                    return False
+
+                else:
+                    if w not in cur.children:
+                        return False
+                    cur = cur.children[w]
+            return cur.isEndNode
+        return dfs(0,self.root)
     
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
