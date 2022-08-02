076f663356908fbf2f8dfd6c726d2c4ba29dce80
saviaga <saviaga@gmail.com>
2022-08-02 13:14:51 -0400

    Time: 16505 ms (40.46%), Space: 65 MB (83.43%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -1,37 +1,49 @@
-class TrieNode():
-    def __init__(self):
-        self.children = collections.defaultdict(TrieNode)
-        self.isWord = False
-    
+
 class WordDictionary(object):
     def __init__(self):
-        self.root = TrieNode()
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
+
 
     def addWord(self, word):
-        node = self.root
-        for w in word:
-            node = node.children[w]
-        node.isWord = True
+        """
+        Adds a word into the data structure.
+        """
+        node = self.trie
 
+        for ch in word:
+            if not ch in node:
+                node[ch] = {}
+            node = node[ch]
+        node['$'] = True
+        
+        
     def search(self, word):
-        node = self.root
-        self.res = False
-        self.dfs(node, word)
-        return self.res
-    
-    def dfs(self, node, word):
-        if not word:
-            if node.isWord:
-                self.res = True
-            return 
-        if word[0] == ".":
-            for n in node.children.values():
-                self.dfs(n, word[1:])
-        else:
-            node = node.children.get(word[0])
-            if not node:
-                return 
-            self.dfs(node, word[1:])
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any letter.
+        """
+        def search_in_node(word, node):
+            for i, ch in enumerate(word):
+                if not ch in node:
+                    # if the current character is '.'
+                    # check all possible nodes at this level
+                    if ch == '.':
+                        for x in node:
+                            if x != '$' and search_in_node(word[i + 1:], node[x]):
+                                return True
+                    # if no nodes lead to answer
+                    # or the current character != '.'
+                    return False
+                # if the character is found
+                # go down to the next level in trie
+                else:
+                    node = node[ch]
+            return '$' in node
+
+        return search_in_node(word, self.trie)
+
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
