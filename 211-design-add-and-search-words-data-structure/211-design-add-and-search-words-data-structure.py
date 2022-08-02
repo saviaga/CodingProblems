1dda06975d2b656dd2ec03baf39b3ac4ab9fd9de
saviaga <saviaga@gmail.com>
2022-08-02 18:40:39 -0400

    Time: 13354 ms (73.02%), Space: 64.1 MB (94.03%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -15,19 +15,19 @@ class WordDictionary(object):
     def search(self, word):
         return self.helper(self.root, word)
     
-    def helper(self, trie, word):
+    def helper(self, node, word):
         if not word:
-            if 'eow' in trie:
+            if 'eow' in node:
                 return True
             else:
                 return False
 
         if word[0] == '.':
-            for c in trie:
-                if c != 'eow' and self.helper(trie[c], word[1:]):
+            for c in node:
+                if c != 'eow' and self.helper(node[c], word[1:]):
                     return True
-        elif word[0] in trie:
-            return self.helper(trie[word[0]], word[1:])
+        elif word[0] in node:
+            return self.helper(node[word[0]], word[1:])
         return False 
     
     #Easy part is space complexity, it is O(M), where M is sum of lengths of all words in our Trie. This is upper bound: in practice it will be less than M and it depends, how much words are intersected. The worst time complexity is also O(M), potentially we can visit all our Trie, if we have pattern like ...... For words without ., time complexity will be O(h), where h is height of Trie. For words with several letters and several ., we have something in the middle.
