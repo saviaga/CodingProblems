bce66ff9564b7c1b8036decabd25dc4cce89ac1d
saviaga <saviaga@gmail.com>
2022-08-03 15:32:10 -0400

    Time: 31 ms (39.09%), Space: 13.4 MB (68.91%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
         m = [0]*len(nums)
         
        
-        if len(nums)<=2:
-            return max(nums)
+        if len(nums)==1:
+            return nums[0]
         m[0] = nums[0]
         m[1] = max(nums[0],nums[1])
         for i in range(2,len(nums)):
         
             m[i] = max((nums[i]+m[i-2]), m[i-1])
         return m[-1]
-                              
+        
+     
