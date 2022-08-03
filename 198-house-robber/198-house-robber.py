7a1d35a6b9231bfe608aea17f1c385d8624075ca
saviaga <saviaga@gmail.com>
2022-08-03 15:59:11 -0400

    Time: 24 ms (69.11%), Space: 13.3 MB (88.96%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -4,16 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        m = [0]*len(nums)
+        
+        
+        def robhelper(newnums):
+            #print(newnums)
+            m = [0]*len(nums)
         
        
-        if len(nums)==1:
-            return nums[0]
-        m[0] = nums[0]
-        m[1] = max(nums[0],nums[1])
-        for i in range(2,len(nums)):
+            if len(nums)==1:
+                return nums[0]
+            m[0] = nums[0]
+            m[1] = max(nums[0],nums[1])
+            for i in range(2,len(nums)):
+
+                m[i] = max((nums[i]+m[i-2]), m[i-1])
+            return m[-1]
         
-            m[i] = max((nums[i]+m[i-2]), m[i-1])
-        return m[-1]
+        
+        
+        return max(robhelper(nums[1:]), robhelper(nums[:-1]))
+    
+    
         
      
