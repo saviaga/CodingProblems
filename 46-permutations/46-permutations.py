a84df3c52baa3633e21e43b2b39a72917d66406c
saviaga <saviaga@gmail.com>
2022-08-04 09:32:19 -0400

    Time: 52 ms (20.02%), Space: 13.7 MB (17.20%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -4,24 +4,38 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        def permuteHelper(numbers, chosen):
-            if len(numbers)==0:
-                res.append(chosen[:])
-                
+        
+        def permuteHelper(nums,chosen):
+            self.calls+=1
+            if len(nums)==0:
+               
+                return res.append(chosen[:])
             else:
-                for i in range(len(nums)):
-                    #chose
-                    c = nums[i]
-                    chosen.append(c)
-                    del nums[i]
+                #ABCD - > Choose A -> permute the rest of the elements BCD
+                for i in range(len(nums)):  
+                    
+                    #choose
+                    element = nums[i]
+                    chosen.append(element) #Choose A
+                    nums.remove(element)  # permute the rest of the elements BCD
                     #explore
-                    permuteHelper(numbers,chosen)
-                    nums.insert(i,c)
-                    #unchoose
+                    permuteHelper(nums,chosen)
+                    #unchose
+                    nums.insert(i,element)  #unchosse c (send back to  string)
                     chosen.pop()
         
-        
-        
-        res = []
+      
+        res = []  
+        self.calls = 0
         permuteHelper(nums,[])
+        print(self.calls)
         return res
+        
+        #perm
+                
+           
+            
+                    
+                   
+                
+                
