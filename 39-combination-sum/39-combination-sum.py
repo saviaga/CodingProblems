e37d1513dab0847e63874270e9dc85396cb60ee4
saviaga <saviaga@gmail.com>
2022-08-04 11:23:06 -0400

    Time: 125 ms (39.96%), Space: 13.7 MB (44.83%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -8,7 +8,6 @@ class Solution(object):
         
         
         def combineHelper(remain_sum,chosen,curr_start):
-            
             if remain_sum==0:
                 res.append(chosen[:])
                 return
@@ -19,8 +18,7 @@ class Solution(object):
                 for i in range(curr_start,len(candidates)):
                     #chose
                     elem = candidates[i]
-                    chosen.append(elem)
-             
+                    chosen.append(elem)         
                     #explore
                     combineHelper(remain_sum-elem,chosen,i) #give this number another chance
                     #unchose
