8a81041ae347d4821454c386423f285621acacb2
saviaga <saviaga@gmail.com>
2022-08-04 12:30:59 -0400

    Time: 434 ms (48.55%), Space: 13.7 MB (71.88%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count=0
+        start=0
+        end = 0
         max_c_ones = 0
-        
-        for n in nums:
-            if n == 1:
-                count+=1
-                max_c_ones=max(max_c_ones,count)
-            else:
-                
-                
-                count=0
+        while start < len(nums) and end< len(nums):
+            #find first 1
+            while start < len(nums) and nums[start] == 0:
+                start+=1
+
+            end = start
+            #find the end of that window of 1s
+            while end < len(nums) and nums[end] == 1:
+                end+=1
+            max_c_ones= max(max_c_ones,end-start)
+            start=end
         return max_c_ones
