5f69d56068f49d3e7376850ba3ca2e305e77d8ae
saviaga <saviaga@gmail.com>
2022-08-04 12:55:16 -0400

    Time: 509 ms (25.52%), Space: 14 MB (15.99%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -7,6 +7,7 @@ class Solution(object):
             #Sliding window
             max_count = 0
             start = 0
+            count = 0
             for end in range (0, len(nums)):
                 if nums[end]==1:                
                     max_count = max(max_count, end-start+1)
