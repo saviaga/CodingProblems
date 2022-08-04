d98927965581a7c6e175a4424fdf49b40f79bddb
saviaga <saviaga@gmail.com>
2022-08-04 12:54:19 -0400

    Time: 575 ms (11.31%), Space: 13.8 MB (37.66%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -4,20 +4,13 @@ class Solution(object):
             :type nums: List[int]
             :rtype: int
             """
-            start=0
-            end = 0
-            max_c_ones = 0
-            while end< len(nums) and start<len(nums):
-           
-                while start<len(nums) and  nums[start] == 0: #move start while there are 0
-                    start+=1
-                
-                end=start
-                #if it is not zero we can start countin 1s
-                while end < len(nums) and  nums[end]==1:
-                    end+=1
-                
-                max_c_ones= max(max_c_ones,end-start)
-                start=end
-            
-            return max_c_ones
+            #Sliding window
+            max_count = 0
+            start = 0
+            for end in range (0, len(nums)):
+                if nums[end]==1:                
+                    max_count = max(max_count, end-start+1)
+                    end = start + 1
+                else:
+                    start = end + 1
+            return max_count
