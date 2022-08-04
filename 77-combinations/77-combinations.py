dd407e62a5436affd59d29b8a6e4c21fccbd21f3
saviaga <saviaga@gmail.com>
2022-08-04 10:08:34 -0400

    Time: 767 ms (31.98%), Space: 15 MB (58.81%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -6,23 +6,18 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         def combineHelper(curr,chosen):
-           
-            
+           # print("chosen ",chosen,"nums ",curr)
             if len(chosen)==k:
                 res.append(chosen[:])
-              
-            else:    
-                for i in range(curr,n+1):
-                    #chose 
-                    chosen.append(i)
-                    
-                    #explore
-                    combineHelper(i+1,chosen)
-                   
-                    #unchoose
-                    chosen.pop()
-                   
-                    
+                return
+            for i in range(curr,n+1):
+                #chose
+                chosen.append(i)
+                #explore
+                combineHelper(i+1,chosen)
+                #unchose
+                chosen.pop()
+        
         res = []
         combineHelper(1,[])
         return res
