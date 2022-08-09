fd4de5c77e62df6193990eb71ad65cb42759b9b3
saviaga <saviaga@gmail.com>
2022-08-09 19:27:00 -0400

    Time: 2551 ms (32.13%), Space: 30 MB (23.76%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -11,6 +11,7 @@ class Solution(object):
         
         #construct dp
         dp = [[False for x in range(subset_sum + 1)] for y in range(n + 1)]
+        
         dp[0][0] = True
         for i in range(1,n+1):
             for j in range(subset_sum + 1):
