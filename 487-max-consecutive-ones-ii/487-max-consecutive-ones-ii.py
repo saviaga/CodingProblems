1136150e224af5764171926980a48f6e25960d1a
saviaga <saviaga@gmail.com>
2022-08-08 20:24:17 -0400

    Time: 300 ms (94.35%), Space: 14.2 MB (6.05%) - LeetHub

--- 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.py
+++ 487-max-consecutive-ones-ii/487-max-consecutive-ones-ii.py
@@ -7,18 +7,15 @@ class Solution(object):
         
         curr_zero = 0
         max_ones = 0
-        start= 0
         can_change = True
-        
+        start = 0
         for end in range(len(nums)):
-            if nums[end]==1:
-                max_ones = max(max_ones,end-start+1)
-            elif can_change:
+           
+            if nums[end]==0 and can_change:
                 can_change = False
-                max_ones = max(max_ones,end-start+1)
                 curr_zero = end
-            else:
+            elif nums[end]==0 and not can_change:
                 start = curr_zero+1
                 curr_zero = end
-                max_ones = max(max_ones,end-start+1)
+            max_ones = max(max_ones,end-start+1)
         return max_ones
