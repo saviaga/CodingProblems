05ade302ce21d4ef184a3822bfb5fe078c8c39a2
saviaga <saviaga@gmail.com>
2022-08-09 19:18:36 -0400

    Time: 3707 ms (14.47%), Space: 29.7 MB (34.61%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -15,12 +15,14 @@ class Solution(object):
         for i in range(n+1):
             for j in range(subset_sum + 1):
                 
-                if i==0:
+                if i==0 or j==0:
                     dp[i][j] = False
-                if j ==0:
-                    dp[i][j] = True 
+               
                 elif nums[i-1] > j: #if i cannot include it, then don't
                     dp[i][j] = dp[i-1][j]
+                    
+                elif nums[i-1] == j:
+                    dp[i][j] = True
                 else:
                     #not include it or include it
                     dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i-1]]
