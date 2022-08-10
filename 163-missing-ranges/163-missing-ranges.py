7497dab76c26575e3027ddc6d5f76ffc07f1e8b8
saviaga <saviaga@gmail.com>
2022-08-09 22:11:22 -0400

    Time: 23 ms (63.64%), Space: 13.5 MB (39.67%) - LeetHub

---
+++ 163-missing-ranges/163-missing-ranges.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def findMissingRanges(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: List[str]
+        """
+        prev = lower-1
+        nums
+        res = []
+        for i in range(len(nums)+1):
+            if i < len(nums):
+                curr = nums[i]
+                
+            else:
+                curr = upper + 1
+          
+
+            if prev+1 < curr-1:
+                res.append(str(prev+1) + "->" + str(curr-1))
+            elif prev+1 == curr-1:
+                 res.append(str(prev+1))
+            
+            prev = curr
+        
+        return res
+        
+        
+
+
+                
+            
+        
