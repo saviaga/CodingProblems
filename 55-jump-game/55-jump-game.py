0a5a6605d6a2f7df66cf21d4266a415d765f0ee4
saviaga <saviaga@gmail.com>
2022-08-10 09:29:55 -0400

    Time: 533 ms (63.19%), Space: 14.6 MB (25.21%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        goal = len(nums)-1 #the goal is the last index
+        for i in range(len(nums)-1,-1,-1):
+            
+            if i + nums[i] >= goal:
+                goal = i
+        return goal ==0
+                
+        
