b736fb9d7cf5e3f2a76bbca9d7bc46514cf433e2
saviaga <saviaga@gmail.com>
2022-08-09 21:21:36 -0400

    Time: 21 ms (95.24%), Space: 13.6 MB (50.79%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -8,16 +8,20 @@ class Solution(object):
         word_set = set(wordDict)
         q = deque()
         visited = set()
-
+        longest = 0
+        for elem in wordDict:
+            longest = max(longest,len(elem))
+        print(longest)
         q.append(0)
         while q:
             start = q.popleft()
             if start in visited:
                 continue
             for end in range(start + 1, len(s) + 1):
-                
+                if len(s[start:end]) > longest:
+                    break
                 if s[start:end] in word_set:
-                  
+                    print(s[start:end])
                     q.append(end)
                     if end == len(s):
                         return True
