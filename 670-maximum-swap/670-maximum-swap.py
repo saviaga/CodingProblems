cabc87f291ffe35d17f7c84b363082bf607b4992
saviaga <saviaga@gmail.com>
2022-08-11 10:58:32 -0400

    Time: 36 ms (15.79%), Space: 13.1 MB (99.60%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def maximumSwap(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num<10:
+            return num
+        #save the largest
+        max_so_far = num
+        
+
+        lstnum = [int(i) for i in str(num)]
+
+        #store indexes in dic
+        
+       
+        
+        for i in range(len(lstnum)-1):
+            max_num = max(lstnum[i+1:])
+            
+            if max_num > lstnum[i]:
+                    
+                    for j in range(len(lstnum)-1, i, -1): #find the index of the max num starting from left
+                        if lstnum[j] == max_num:
+                            break
+                    lstnum[i],lstnum[j] = lstnum[j],lstnum[i]
+                    break
+                                    
+        return "".join([str(i) for i in lstnum])
+        
+            
+        
+            
+        
+        
