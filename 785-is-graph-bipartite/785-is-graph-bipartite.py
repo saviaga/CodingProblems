04342c4a4c5bfcc8b5be7308f80c4e4452c5e891
saviaga <saviaga@gmail.com>
2022-08-15 10:31:05 -0400

    Time: 279 ms (25.32%), Space: 14.2 MB (12.03%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isBipartite(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: bool
+        """
+        N= len(graph)
+        col = [-1]*N
+        
+        for i in range(N):
+            if col[i]!=-1:
+                continue
+            q = deque()
+            q.append((i,0))
+
+            while q:
+                    node,color = q.popleft()
+                    if col[node] == -1:
+                        col[node] = color
+                        for ch in graph[node]:
+                            q.append((ch,color^1))
+                    if col[node]!=color:
+                        return False
+        return True
+            
+        
