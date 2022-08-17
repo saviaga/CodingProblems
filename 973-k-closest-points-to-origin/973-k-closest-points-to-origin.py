c5fe0bff5657b9eb69a339f6c4bad8f4336d4a55
saviaga <saviaga@gmail.com>
2022-08-17 17:32:05 -0400

    Time: 1797 ms (7.60%), Space: 20.3 MB (81.06%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,26 +1,16 @@
-class Solution(object):
-    def kClosest(self, points, k):
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
         def get_eucledian(coord):
             x1,x2=coord
             
             return math.sqrt((x1-0)**2+(x2-0)**2)
 
-        
-        distances = []
+        import queue
+        distances = queue.PriorityQueue()
         for p in points:
-            distances.append([get_eucledian(p),p])
+            distances.put((get_eucledian(p),p))
         
-        distances.sort()
         
-        return [elem[1] for elem in distances[:k]]
+        return [distances.get()[1] for _ in range(k)]
    
-        
-        
-                             
-
-            
