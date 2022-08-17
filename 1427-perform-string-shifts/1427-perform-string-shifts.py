e6a1454244b152210e20e6869b660c2f0f4f1b66
saviaga <saviaga@gmail.com>
2022-08-17 13:39:01 -0400

    Time: 21 ms (75.00%), Space: 13.4 MB (96.88%) - LeetHub

---
+++ 1427-perform-string-shifts/1427-perform-string-shifts.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def stringShift(self, s, shift):
+        """
+        :type s: str
+        :type shift: List[List[int]]
+        :rtype: str
+        """
+        val = 0
+        for elem in shift:
+            direction,places = elem
+            if direction==0:
+                val+= -places
+            else:
+                val+= places
+                
+        rotation = val%len(s)
+        print(rotation)
+        res=''
+        if rotation<0: #rotate to the left
+            res= s[rotation:] + s[:rotation]  
+        else: #rotate to the right
+            res =  s[-rotation:] + s[:-rotation] 
+            
+        return res
+       
+     
+            
+        
