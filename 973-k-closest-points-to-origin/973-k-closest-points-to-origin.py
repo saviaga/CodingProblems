0ed9ec53a49365ee84c430ce23e08f0762f64fc2
saviaga <saviaga@gmail.com>
2022-08-18 09:22:06 -0400

    Time: 1687 ms (11.65%), Space: 20.5 MB (39.01%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,23 +1,31 @@
+import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
         def get_eucledian(coord):
-            x1,x2=coord
-            
+            x1,x2=coord            
             return math.sqrt((x1-0)**2+(x2-0)**2)
-
-       
+            
         distances = []
+        
         for p in points:
             distances.append((get_eucledian(p),p))
-        heapq.heapify(distances)
         
-   
-        return [heapq.heappop(distances)[1]  for _ in range(k)]
+        #print(distances)
+        top_k = []
+        for elem in distances:
+            if len(top_k)<k:
+                heapq.heappush(top_k, (elem[0]*(-1), elem[1]))
+            else:
+                heapq.heappush(top_k, (elem[0]*(-1), elem[1]))
+                heapq.heappop(top_k)
+            #print(top_k)
+        
+        return [elem[1] for elem in top_k]
 
-        #time: O(N + KlogN) -> N to get the distances, and KlogN for the heap (each pop is logN and there are K elements)
-    
-        #space O(N)
+
+
+        
   
             
    
