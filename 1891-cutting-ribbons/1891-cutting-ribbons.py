2a92fc7869afc137a2480ebfe907fa2a6d063a47
saviaga <saviaga@gmail.com>
2022-08-20 12:02:03 -0400

    Time: 1616 ms (85.39%), Space: 28.5 MB (15.12%) - LeetHub

--- 1891-cutting-ribbons/1891-cutting-ribbons.py
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -5,25 +5,17 @@ class Solution:
         for ribbon in ribbons:
             cut+=ribbon//lenght
         return cut>=k
-    
+
     def maxLength(self, ribbons: List[int], k: int) -> int:
-        
-        
         #pick a number from 1 to max(ribbons)->binary search
         #check if it all three can bit cut in that lenght (basically all are smaller or equial that that
         #keep looking
-        
-        
-        
-        
         start = 1
         end = max(ribbons)
         
         while start<=end:
             mid = start + (end-start)//2
-            
             if self.can_cut(ribbons,k,mid):
-                
                 start = mid+1
             else:
                 end = mid-1
