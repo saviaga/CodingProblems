69da52c381d2e466c0c6899a489e40ca7129ad19
saviaga <saviaga@gmail.com>
2022-08-20 13:59:17 -0400

    Time: 843 ms (5.04%), Space: 14.2 MB (76.91%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -3,12 +3,12 @@ class Solution:
         
             max_count = 0
             start = 0
-            count = 0
             for end in range (0, len(nums)):
                 if nums[end]==1:                
                     max_count = max(max_count, end-start+1)
                    
                 else:
                     start = end + 1
+                   
             return max_count
         
