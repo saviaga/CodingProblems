c83bf0aeac8fadfa4f3ff8ab63cd8401e2fe257c
saviaga <saviaga@gmail.com>
2022-08-20 10:12:39 -0400

    Time: 65 ms (30.70%), Space: 15 MB (19.04%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -10,28 +10,37 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         
+        if head==None:
+            return head
         
         curr = head
-        old_to_new = {None:None}
         
-        #first pass store values in dict
+        
+        #first pass insert the new_nodes A->A'
         while curr:
-            #new node
             new_node = Node(curr.val)
-            old_to_new[curr] = new_node
-            curr = curr.next
-       
+            new_node.next = curr.next
+            curr.next = new_node
+            curr=new_node.next
+            
+        #link the random pointers to the new nodes
         curr = head
         while curr:
-            copy = old_to_new[curr]
-            copy.next = old_to_new[curr.next]
-            copy.random = old_to_new[curr.random]
-            curr = curr.next
+            curr.next.random = curr.random.next if curr.random else None
+            curr = curr.next.next
             
+        #unlink the old ones 
+        old = head  # A->B->C
+        new = head.next  # A'->B'->C'
+        head_new = head.next
+        while old:
+            old.next = old.next.next
+            new.next = new.next.next if new.next else None
+            old = old.next
+            new=new.next
+        return head_new
             
-            
-            
-        return old_to_new[head] 
+        
         
         
         
