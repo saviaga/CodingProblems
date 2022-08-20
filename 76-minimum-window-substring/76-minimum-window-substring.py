3099bc12a7cf3a0f81030c98987cf349e72f7dd9
saviaga <saviaga@gmail.com>
2022-08-20 17:19:05 -0400

    Time: 103 ms (91.32%), Space: 14.7 MB (36.91%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -0,0 +1,45 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        
+        countT={}
+        window={}
+        start = 0
+        res = [-1,-1]
+        reslen = float('inf')
+        
+        
+        #save freq of T
+        for c in t:
+            countT[c] = 1+ countT.get(c,0)
+        have,need =0,len(countT)
+        print((countT))
+        print(len(countT))
+        for end in range(len(s)):
+            ch = s[end]
+            window[ch]=1+window.get(ch,0)
+            
+            if ch in countT and window[ch]==countT[ch]:
+                have+=1
+            while have==need:
+                window_size = end-start+1
+                if window_size < reslen:
+                    res=[start,end]
+                    reslen = window_size
+                window[s[start]]-=1
+                if s[start] in countT and window[s[start]] < countT[s[start]]:
+                    have-=1
+                start+=1
+        start,end = res
+        
+        return s[start:end+1] if reslen!=float('inf') else ""
+                
+                
+                
+            
+            
+            
+            
+        
+        
+        
