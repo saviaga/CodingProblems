d091526a2131928b0a0893dec66f2654a61b9dc6
saviaga <saviaga@gmail.com>
2022-08-20 20:50:43 -0400

    Time: 29 ms (96.57%), Space: 14 MB (19.97%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -2,6 +2,8 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         
         def helper(x,n):
+           
+            if x==0: return 0
             if n==0: return 1
             
             temp = helper(x,n//2)
