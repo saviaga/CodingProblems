79e7cdc7b8ed641ffb39424da34399d35ee9bc95
saviaga <saviaga@gmail.com>
2022-08-24 21:28:34 -0400

    Time: 124 ms (77.69%), Space: 14.2 MB (47.64%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -6,23 +6,21 @@ class Solution(object):
         """        
         def valid_palindromehelper(s,start,end):
             while start<=end:
-                if s[start]==s[end]:
-                    start+=1
-                    end-=1
-                else: 
+                if s[start]!=s[end]:
                     return False
+                start+=1
+                end-=1
             return True
             
         start = 0
         end= len(s)-1
         while start<=end:
-            if s[start]==s[end]:
-                start+=1
-                end-=1
-            else:
+            if s[start]!=s[end]:
                 left = valid_palindromehelper(s,start+1,end)
                 right = valid_palindromehelper(s,start,end-1)
                 return left or right
+            start+=1
+            end-=1
         return True
             
             
