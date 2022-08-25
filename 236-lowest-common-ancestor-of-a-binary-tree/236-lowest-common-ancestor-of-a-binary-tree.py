0699a15292bc93636333ba7c56a965ba69b40a1d
saviaga <saviaga@gmail.com>
2022-08-25 10:56:20 -0400

    Time: 105 ms (59.19%), Space: 18.7 MB (92.18%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,37 +1,49 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        #1.store all the prev parents in a dict
         
         stack = [root]
-        dic_ans = {root:None}
-        LCAs = set()
-        while p not in dic_ans or q not in dic_ans:
-            curr = stack.pop()
-            if curr.left:
-                dic_ans[curr.left] = curr
-                stack.append(curr.left)
-            if curr.right:
-                dic_ans[curr.right] = curr
-                stack.append(curr.right)
-
-        while p:
-            LCAs.add(p)
-            p = dic_ans[p]
-            
-        while q not in LCAs:
-            q = dic_ans[q]
-        return q
         
-                
+        dic_val = {root:None}
+        
+
+        while stack:
+                curr = stack.pop()
+                if curr.left:
+                    stack.append(curr.left)
+                    dic_val[curr.left]=curr
+                if curr.right:
+                    stack.append(curr.right)
+                    dic_val[curr.right]=curr
+        
+        # Process all ancestors for node p using parent pointers.
+        #start with p because a node can be its own ancestor
+        p_ancestors = set()
+        while p:
+            p_ancestors.add(p)
+            p=dic_val[p]
+           
+        # The first ancestor of q which appears in
+        # p's ancestor set() is their lowest common ancestor.
+        while q not in p_ancestors:
+            q=dic_val[q]
+        
+        return q
+            
+
+                  
+            
+                    
+        
+       
+        
+        
+        
