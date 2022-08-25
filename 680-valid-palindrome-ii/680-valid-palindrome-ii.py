e5be76c0b84451048f3c90c87c39576d71abbbce
saviaga <saviaga@gmail.com>
2022-08-24 21:27:23 -0400

    Time: 149 ms (61.11%), Space: 13.8 MB (99.50%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,36 +1,30 @@
 class Solution(object):
-    def validatepalindrome(self,s,start,end):
-
-            while start<=end:
-                if s[start]==s[end]:
-                    start+=1
-                    end-=1
-                else:
-                    return False
-            return True
     def validPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
-        """
-        
-    
-    
-        start = 0
-        end = len(s)-1
-        
-        while start<=end:
+        """        
+        def valid_palindromehelper(s,start,end):
+            while start<=end:
+                if s[start]==s[end]:
+                    start+=1
+                    end-=1
+                else: 
+                    return False
+            return True
             
+        start = 0
+        end= len(s)-1
+        while start<=end:
             if s[start]==s[end]:
                 start+=1
                 end-=1
             else:
-                #look withouth elem in right
-                left = self.validatepalindrome(s,start+1,end)
-                right = self.validatepalindrome(s,start,end-1)
-                #look without elem in left
+                left = valid_palindromehelper(s,start+1,end)
+                right = valid_palindromehelper(s,start,end-1)
                 return left or right
-                
         return True
             
             
+
+        
