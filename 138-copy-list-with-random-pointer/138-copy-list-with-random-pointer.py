661bcca281252b172ce7910f19b258931b2bf31b
saviaga <saviaga@gmail.com>
2022-08-25 16:55:36 -0400

    Time: 57 ms (49.90%), Space: 14.8 MB (98.98%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -17,26 +17,32 @@ class Solution:
         #first pass insert the new_nodes A->A'
         while curr:
             new_node = Node(curr.val)
-            new_node.next = curr.next
+            new_node.next =curr.next
             curr.next = new_node
-            curr=new_node.next
+            curr = new_node.next
+            
+        
             
         #link the random pointers to the new nodes
-        curr = head
+        curr= head
         while curr:
-            curr.next.random = curr.random.next if curr.random else None
+            #the cloned random
+            curr.next.random = curr.random.next if curr.random else None 
             curr = curr.next.next
-            
+        
+              
         #unlink the old ones 
-        old = head  # A->B->C
-        new = head.next  # A'->B'->C'
-        head_new = head.next
+        old = head# A->B->C
+        new = head.next# A'->B'->C'
+        new_head = head.next
+        
         while old:
-            old.next = old.next.next
-            new.next = new.next.next if new.next else None
+            old.next = new.next
+            new.next = old.next.next if old.next else None
             old = old.next
             new=new.next
-        return head_new
+        return new_head
+     
             
         
         
