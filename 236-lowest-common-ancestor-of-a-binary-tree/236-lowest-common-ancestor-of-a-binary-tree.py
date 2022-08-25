3cab8de5752972a6fbc365eb2323d03d9f68798b
saviaga <saviaga@gmail.com>
2022-08-25 10:59:26 -0400

    Time: 136 ms (24.90%), Space: 18.7 MB (92.18%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -11,32 +11,27 @@ class Solution:
         #1.store all the prev parents in a dict
         
         stack = [root]
+        dict_val = {root:None}
         
-        dic_val = {root:None}
-        
-
         while stack:
-                curr = stack.pop()
-                if curr.left:
-                    stack.append(curr.left)
-                    dic_val[curr.left]=curr
-                if curr.right:
-                    stack.append(curr.right)
-                    dic_val[curr.right]=curr
+            curr = stack.pop()
+            if curr.left:
+                stack.append(curr.left)
+                dict_val[curr.left]=curr
+            if curr.right:
+                stack.append(curr.right)
+                dict_val[curr.right]=curr
         
-        # Process all ancestors for node p using parent pointers.
-        #start with p because a node can be its own ancestor
-        p_ancestors = set()
+        ancestors = set()
         while p:
-            p_ancestors.add(p)
-            p=dic_val[p]
-           
-        # The first ancestor of q which appears in
-        # p's ancestor set() is their lowest common ancestor.
-        while q not in p_ancestors:
-            q=dic_val[q]
-        
+            ancestors.add(p)
+            p=dict_val[p] 
+            
+        while q not in ancestors:
+            q = dict_val[q]
+            
         return q
+            
             
 
                   
