7acaf4750d886495e87467ddd2294edda269e14f
saviaga <saviaga@gmail.com>
2022-08-24 22:43:30 -0400

    Time: 61 ms (30.40%), Space: 13.9 MB (80.51%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -7,31 +7,33 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        if root == None:
-            return
+        min_col=0
+        max_col=0
+        if root ==None:
+            return 
         
+        queue = deque([(root,0)])
         
-        queue=collections.deque([[root,0]])
         res = defaultdict(list)
-        min_col = 0
+        min_col =0
         max_col = 0
         all_levels = []
         while queue:
-            for _ in range(len(queue)):
-                curr,col = queue.popleft()
-                min_col = min(min_col,col)
-                max_col = max(max_col,col)
-                res[col].append(curr.val)
-                if curr.left:
-                    queue.append([curr.left,col-1])
-                if curr.right:
-                    queue.append([curr.right,col+1])
-        
+            curr,col = queue.popleft()
+            res[col].append(curr.val)
+            min_col = min(min_col,col)
+            max_col = max(max_col,col)
+            if curr.left:
+                queue.append((curr.left,col-1))
+            if curr.right:
+                queue.append((curr.right,col+1))
         
         for i in range(min_col,max_col+1):
             all_levels.append(res[i])
-            
         return all_levels
         
         
+            
+            
+            
         
