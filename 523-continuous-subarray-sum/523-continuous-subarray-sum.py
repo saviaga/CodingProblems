7eb0063562f41f42c72c12329164490c9fcb4b87
saviaga <saviaga@gmail.com>
2022-08-25 15:24:06 -0400

    Time: 1513 ms (42.44%), Space: 33.2 MB (69.48%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -1,23 +1,21 @@
-class Solution(object):
-    def checkSubarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        
         remainder_dic = {0:-1}
         
         total = 0
         for i, n in enumerate(nums):
             total+=n
+            
             remainder = total%k
-            if remainder not in remainder_dic:
+           
+            if remainder not in remainder_dic: #remainder not in dic
                 remainder_dic[remainder]  = i #we store index
+            
+            #remainder in dic, check that size>2
             elif i - remainder_dic[remainder] > 1: #if at least 2 elems
                 return True
+            
         return False
             
-            
-            
-        
         
