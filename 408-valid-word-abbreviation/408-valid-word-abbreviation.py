f6f32ef4a24bcebbd824fc093f06a325abdbb048
saviaga <saviaga@gmail.com>
2022-08-25 09:46:34 -0400

    Time: 66 ms (10.32%), Space: 13.9 MB (81.88%) - LeetHub

--- 408-valid-word-abbreviation/408-valid-word-abbreviation.py
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.py
@@ -1,32 +1,32 @@
-class Solution(object):
-    def validWordAbbreviation(self, word, abbr):
-        """
-        :type word: str
-        :type abbr: str
-        :rtype: bool
-        """
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        
+        
         i=0
         j=0
-        
         while i<len(abbr) and j<len(word):
-            if abbr[i].isalpha():             
+            #if the chaacter is alphanumeric
+            if abbr[i].isalpha():
+                #if  chars at same position are different
                 if abbr[i]!=word[j]:
                     return False
+                #if they are equial continue comparing
                 i+=1
                 j+=1
             else:
-                
+                #if the char is a number
+                #check if is a leading zero, if it is return False
                 if abbr[i]=='0':
                     return False
-                temp = ''
+                #if it is not a leading zero
                 
+                    #read the number
+                temp=''
                 while i<len(abbr) and abbr[i].isdigit():
                     temp+=abbr[i]
-                
                     i+=1
                 j+=int(temp)
         return i==len(abbr) and j==len(word)
-                
-                    
+            
                 
         
