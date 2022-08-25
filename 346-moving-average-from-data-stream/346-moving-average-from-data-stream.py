649e97d750274c2becb646a912b42d3c9dc141ba
saviaga <saviaga@gmail.com>
2022-08-25 11:39:22 -0400

    Time: 150 ms (30.69%), Space: 17.2 MB (80.11%) - LeetHub

--- 346-moving-average-from-data-stream/346-moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.py
@@ -14,9 +14,8 @@ class MovingAverage:
         tail = 0
         if self.count > self.size:
             tail = self.queue.popleft()
-            self.window_sum = self.window_sum  -  tail
         else:
-            self.window_sum = self.window_sum  + val
+            self.window_sum = self.window_sum  + val -  tail
 
         return sum(self.queue)/min(self.size,self.count)
         
