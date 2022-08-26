686dec8a127d38235e10dfcbe285b1afc1470213
saviaga <saviaga@gmail.com>
2022-08-26 10:38:55 -0400

    Time: 109 ms (58.56%), Space: 14 MB (94.26%) - LeetHub

--- 636-exclusive-time-of-functions/636-exclusive-time-of-functions.py
+++ 636-exclusive-time-of-functions/636-exclusive-time-of-functions.py
@@ -2,27 +2,26 @@ class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         
         lastlogTime = 0
-        ans = [0]*n
-        stack = []
+        ans=[0]*n
+        stack=[]
         
         for log in logs:
-            #actions[0]->id, actions[1]=>start/end, actions[2]->logtime
-            log_id,status,timestamp = log.split(':')
-            log_id,timestamp = int(log_id),int(timestamp)
-
-            if status=='start':
-                
+            log_id,action,timestamp = log.split(':')
+            log_id = int(log_id)
+            timestamp=int(timestamp)
+            
+            if action=='start':
                 if stack:
                     ans[stack[-1]]+=timestamp-lastlogTime
                 lastlogTime = timestamp
                 stack.append(log_id)
             else:
-                    timestamp=timestamp+1 #to count for ending timestap at end of the time    
-                    ans[stack.pop()]+=timestamp-lastlogTime     
-                    lastlogTime = timestamp
-                            
+                timestamp=timestamp+1
+                ans[stack.pop()]+=timestamp-lastlogTime
+                lastlogTime=timestamp
         return ans
-                    
+            
+        
                 
                 
                 
