3857c27b10494242e87676ee02ae15c8b8c0c47b
saviaga <saviaga@gmail.com>
2022-08-26 19:30:11 -0400

    Time: 39 ms (92.14%), Space: 14.1 MB (10.49%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def groupStrings(self, strings):
-        """
-        :type strings: List[str]
-        :rtype: List[List[str]]
-        """
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        
         def get_key(string):
             key = tuple()
-            for i in range(1,len(string)):
-                 key+=((ord(s[i])-ord(s[i-1]))%26,)
+            key = tuple((ord(s[i])-ord(s[i-1]))%26  for i in range(1,len(string)))
+            
             return key
                 
      
