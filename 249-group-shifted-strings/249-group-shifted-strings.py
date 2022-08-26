194bb20c95e76f16581d4a50583e74ff0be4fda3
saviaga <saviaga@gmail.com>
2022-08-26 19:48:10 -0400

    Time: 64 ms (36.72%), Space: 13.9 MB (86.71%) - LeetHub

--- 249-group-shifted-strings/249-group-shifted-strings.py
+++ 249-group-shifted-strings/249-group-shifted-strings.py
@@ -2,10 +2,12 @@ class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         
         def get_key(string):
-            key = tuple()
+            key = []
             
             for i in range(1,len(string)):
-                key+=(str((ord(s[i])-ord(s[i-1]))%26),) #to count for ord(a)-ord(z)=-25%26=1
+                key.append(str((ord(s[i])-ord(s[i-1]))%26),) #to count for ord(a)-ord(z)=-25%26=1
+            key= ",".join(key)
+            
             return key
                 
      
