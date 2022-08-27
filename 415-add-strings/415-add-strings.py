24a29282d6303cb5d8d9fefd6e6f7cd38eadfafa
saviaga <saviaga@gmail.com>
2022-08-26 21:51:56 -0400

    Time: 56 ms (66.72%), Space: 14.3 MB (25.08%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -3,7 +3,7 @@ class Solution:
         
         
         
-        res = []
+        res = deque()
         a = len(num1) - 1
         b = len(num2) - 1
         curr_sum = 0
@@ -11,19 +11,20 @@ class Solution:
         curr_a,curr_b = 0,0
         
         while a>=0 or b>=0:
-            curr_a = ord(num1[a]) - ord('0') if a>=0 else 0
+            #ord("1")->49 - ord("0")->48 just convert thhe string 1 to the correct integer number 1 
+            curr_a = ord(num1[a]) - ord('0') if a>=0 else 0 #because if one number is bigger then indices would go to negative
             curr_b = ord(num2[b]) - ord('0') if b>=0 else 0
             
             curr_sum = carry + curr_a + curr_b 
-            res.append(str(curr_sum %10))
+            res.appendleft(str(curr_sum %10))
             carry = curr_sum //10
             a-=1
             b-=1
             
         if carry:
-            res.append(str(carry))
+            res.appendleft(str(carry))
             
-        return "".join(reversed(res))
+        return "".join(res)
             
             
            
