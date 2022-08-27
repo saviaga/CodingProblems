09ff0c0fabc6f3f92c1065d86d650713f95dce43
saviaga <saviaga@gmail.com>
2022-08-26 21:15:01 -0400

    Time: 35 ms (95.48%), Space: 15.3 MB (69.48%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -9,40 +9,39 @@ class Node:
 
 class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        
+        def inorder_helper(node):
+            if node==None:
+                return
+
+            #go left
+            inorder_helper(node.left)
+
+            #process current
+            if not self.last:
+                self.first = node
+            else:
+                node.left = self.last
+                self.last.right = node
+            self.last = node
+
+            #go rignt
+            inorder_helper(node.right)
+
         if root==None:
             return None
         
         self.first=None 
         self.last=None
         
-        self.inorder_helper(root)
+        inorder_helper(root)
         
         self.first.left = self.last
         self.last.right = self.first
         
         return self.first
     
-    def inorder_helper(self,node):
-        if node==None:
-            return
-        
-        #go left
-        self.inorder_helper(node.left)
-        
-        #process current
-        if not self.last:
-            self.first = node
-        else:
-            node.left = self.last
-            self.last.right = node
-        self.last = node
-        
-        #go rignt
-        self.inorder_helper(node.right)
         
         
         #TimeO(N)
-        #Space O(N) inbalanced tree (recursive stack) or O(logN) best case, the height of the tree
-        
-        
-        
+        #Space O(N) inbalanced tree (recursive stack) or O(logN) best case, the height of the tree        
