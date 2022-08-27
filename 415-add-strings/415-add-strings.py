b84ac72ba515aaa60aefe8acdab1d13e6e7f9ddc
saviaga <saviaga@gmail.com>
2022-08-26 21:59:23 -0400

    Time: 90 ms (18.77%), Space: 14.4 MB (18.40%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -2,33 +2,26 @@ class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         
         
-        
         res = deque()
-        a = len(num1) - 1
-        b = len(num2) - 1
+        i=len(num1)-1
+        j=len(num2)-1
         curr_sum = 0
         carry = 0
-        curr_a,curr_b = 0,0
-        
-        while a>=0 or b>=0:
-            #ord("1")->49 - ord("0")->48 just convert thhe string 1 to the correct integer number 1 
-            curr_a = ord(num1[a]) - ord('0') if a>=0 else 0 #because if one number is bigger then indices would go to negative
-            curr_b = ord(num2[b]) - ord('0') if b>=0 else 0
+        while i>=0 or j>=0:
             
-            curr_sum = carry + curr_a + curr_b 
-            res.appendleft(str(curr_sum %10))
-            carry = curr_sum //10
-            a-=1
-            b-=1
+            curr_n1 = ord(num1[i]) - ord("0") if i>=0 else 0
+            curr_n2 = ord(num2[j]) - ord("0") if j>=0 else 0
+            
+            curr_sum = carry + curr_n1 + curr_n2
+            
+            res.appendleft(str(curr_sum%10))
+            carry = curr_sum//10
+            
+            i-=1
+            j-=1
             
         if carry:
             res.appendleft(str(carry))
             
         return "".join(res)
-            
-            
-           
-            
         
-        
-       
