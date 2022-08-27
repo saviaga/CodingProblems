3c3cb82c182283a77029282a0870ed66dd2f5ea7
saviaga <saviaga@gmail.com>
2022-08-26 20:23:47 -0400

    Time: 49 ms (49.85%), Space: 13.9 MB (56.99%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
@@ -7,9 +7,8 @@ class Solution:
         for p in s:
             if p=='(':
                 open_p+=1
-            elif p==')':
-                if open_p>0:
+            elif open_p>0:
                     open_p-=1
-                else:
+            else:
                     need_c+=1
         return open_p+need_c
