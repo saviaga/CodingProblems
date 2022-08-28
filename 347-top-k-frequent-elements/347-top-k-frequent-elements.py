555b1844df90d4e2d9a65da38c7a34b6a29e8793
saviaga <saviaga@gmail.com>
2022-08-28 12:59:50 -0400

    Time: 194 ms (28.43%), Space: 18.7 MB (71.74%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,27 +1,14 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        freq = {}
-        res = []
-        for elem in nums:
-            
-            freq[elem] = freq.get(elem,0)+1  #time = O(N)  Space = 0(N)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        myheap = []
-        for i,value in freq.items():
-            heapq.heappush(myheap,(-1*value,i)) #(nlogn) #time O(NlogK) i keep heap of size K , here i keep of size N so O(Nlogn) Space O(k) or O(N)
-                    
-            
-        print(myheap)  #KlogK if k elements in heap here KlogN soace O(1)
-        for _ in range(k):
-            freq,num = heapq.heappop(myheap)
-            res.append(num)
-        return res
+        occurrences = collections.Counter(nums)
+        heap = []
+        print(occurrences)
         
-        
+        for key,value in occurrences.items():
             
+            heapq.heappush(heap,(value,key))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        return [elem[1] for elem in heap]
         
