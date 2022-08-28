5a6cdfdd72aa6fa094b77ac50a87da1ed3b0db44
saviaga <saviaga@gmail.com>
2022-08-27 21:33:01 -0400

    Time: 67 ms (69.95%), Space: 14.7 MB (41.23%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,21 +1,15 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         
         start = 0
-        end = len(nums)-1
+        end = len(nums)
         
-        while start <= end:
-            middle = start + (end-start)//2
-            if nums[middle] == target:
-                return middle
-            elif nums[middle] < target:
-                start = middle + 1
-            elif nums[middle] > target:
-                end = middle - 1
+        while start < end:
+            mid = start + (end-start)//2
+            if nums[mid] >= target:
+                end = mid 
+            else:
+                start = mid + 1
+            
         return start
         
