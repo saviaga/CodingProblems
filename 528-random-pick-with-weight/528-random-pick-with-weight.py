4738d63f7ebd657887bec62869c9a6624ceb4097
saviaga <saviaga@gmail.com>
2022-08-28 12:50:16 -0400

    Time: 392 ms (46.16%), Space: 18.7 MB (23.61%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.py
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -1,28 +1,30 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.prefix_sums=[]
-        total = 0
-        for weight in w:
-            total+=weight
-            self.prefix_sums.append(total)
-        self.total = total
-
-      
-    def pickIndex(self) -> int:
-        target = random.randint(1,self.total)
-        start = 0
-        end= len(self.prefix_sums)-1
-        while start< end:
-            mid =(start+end)//2
-            if self.prefix_sums[mid] < target:
-                start = mid+1
-            elif self.prefix_sums[mid] >= target:
-                end= mid
-        return start
-       
-       
         
+        self.prefix_sum = []
+        self.total = 0
+        for elem in w:
+            self.total+=elem
+            self.prefix_sum.append(self.total)
+        print(self.prefix_sum)
+        
+
+    def pickIndex(self) -> int:
+        choice = random.randint(1,self.total)
+        start = 0
+        end = len(self.prefix_sum)-1
+        
+        while start < end:
+            mid = (start+end)//2
+            if self.prefix_sum[mid] < choice:
+                start = mid+1
+            else:
+                end = mid
+        return start
+               
+            
+                
 
 
 # Your Solution object will be instantiated and called as such:
