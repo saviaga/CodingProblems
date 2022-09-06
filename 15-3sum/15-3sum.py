965e6633ba708d14ab214c76c0e3c5bd99324698
saviaga <saviaga@gmail.com>
2022-09-06 19:06:35 -0400

    Time: 927 ms (80.66%), Space: 18.2 MB (39.32%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    #[-1,0,1,2,-1,-4]
+    #[-1,-1,0,1,2]
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        stack = []
+        nums.sort()
+        for i in range(len(nums)):
+            #check two numbers that sum to the complement of nums
+            if i> 0 and nums[i]==nums[i-1]:
+                continue
+            s=i+1
+            e = len(nums)-1
+
+            while s<e:
+                    if nums[i] + nums[s] + nums[e] > 0:
+                        e-=1
+                    elif  nums[i] + nums[s] + nums[e] < 0:
+                        s+=1
+                    else:
+                        stack.append([nums[i],nums[s],nums[e]])
+                        s+=1
+                        while nums[s]==nums[s-1] and s<e:
+                            s+=1
+                            
+        return stack
+        
