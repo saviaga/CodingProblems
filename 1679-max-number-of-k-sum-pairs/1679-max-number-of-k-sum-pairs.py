4039cc0ee231d2ad873a92922af92ef1ce580456
saviaga <saviaga@gmail.com>
2022-09-07 14:55:09 -0400

    Time: 1488 ms (9.33%), Space: 26.3 MB (91.98%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -8,14 +8,14 @@ class Solution:
         e = len(nums)-1
         
         while s<e:
-            if nums[s] + nums[e] <k:
-                s+=1
-            elif  nums[s] + nums[e] >k:
+            if nums[s] + nums[e] > k:
                 e-=1
+            elif nums[s] + nums[e] < k:
+                s+=1
             else:
                 count+=1
                 s+=1
                 e-=1
-        
+                
         return count
         
