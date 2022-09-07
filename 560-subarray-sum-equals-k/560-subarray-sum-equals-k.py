8ba81eb22f6285817d76e6e18b8a80e2a8d05a3e
saviaga <saviaga@gmail.com>
2022-09-07 17:10:25 -0400

    Time: 779 ms (5.06%), Space: 16.6 MB (74.75%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,25 +1,26 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
         
-        dic_sum = {0:1}
-        cum_sum = 0
         
-        for elem in nums:
-            cum_sum+=elem
-            complement = cum_sum-k
-            if complement in dic_sum:
-                count+=dic_sum[complement]
-            
-            if cum_sum in dic_sum:
-                dic_sum[cum_sum]+=1
-            else:dic_sum[cum_sum]=1
         
+        curr_sum = 0
+        counter = 0
+        dic_nums = {0:1}
+        for i in range(len(nums)):
+            curr_sum+=nums[i]
+            complement = curr_sum-k
+            if complement in dic_nums:
+                counter+=dic_nums[complement]
            
-        return count
-    
+            if curr_sum in dic_nums:
+                dic_nums[curr_sum]+=1
+            else:
+                dic_nums[curr_sum]=1
                 
             
-                
-                
             
+            
+        return counter
+            
+        
+        
