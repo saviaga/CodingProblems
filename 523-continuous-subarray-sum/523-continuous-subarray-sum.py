31807da1ba0995df4eed023f169473df87bc7331
saviaga <saviaga@gmail.com>
2022-09-07 17:45:24 -0400

    Time: 1098 ms (84.48%), Space: 33.3 MB (39.41%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -4,13 +4,15 @@ class Solution:
         dic_rem = {0:-1}
         curr_sum = 0
         
-        for end in range(len(nums)):
-            curr_sum+=nums[end]
+        for i in range(len(nums)):
+            curr_sum+=nums[i]
             
             remainder = curr_sum%k
+
             if remainder not in dic_rem:
-                dic_rem[remainder]=end
-            elif end - dic_rem[remainder] > 1:
+                dic_rem[remainder]=i
+            elif i - dic_rem[remainder] > 1:
+                   
                     return True 
             
         return False
