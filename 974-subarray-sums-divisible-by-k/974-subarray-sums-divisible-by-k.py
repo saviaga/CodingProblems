15000408aa75e8d2f3783a2e46f05d90f7bdd9ff
saviaga <saviaga@gmail.com>
2022-09-07 18:32:33 -0400

    Time: 756 ms (5.08%), Space: 18.9 MB (68.07%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.py
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.py
@@ -3,19 +3,20 @@ class Solution:
         
         count=0
         curr_sum=0
-        remainder = 0
+
         dic_remainder = {0:1}
         for i in range(len(nums)):
             curr_sum+=nums[i]
             remainder = curr_sum%k
             if remainder in dic_remainder:
-                count+=dic_remainder[remainder]
-            if remainder not in dic_remainder:
                 
+                count+=dic_remainder[remainder]
+                
+            if remainder not in dic_remainder:                
                 dic_remainder[remainder] = 1   
             else:
                 dic_remainder[remainder]+=1
-                
+
         return count
             
             
