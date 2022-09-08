e9078401b72815f1ea31fb72380ebb8a95d84cb5
saviaga <saviaga@gmail.com>
2022-09-07 21:16:48 -0400

    Time: 61 ms (46.43%), Space: 13.8 MB (85.80%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,40 +1,23 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
+        mn=m+n-1
+        m=m-1
+        n=n-1
         
-        idx_new = len(nums1)-1
-        m = m -1
-        n = n -1
-        
-        while m >= 0 and n>=0:
+        while m >= 0 and n >= 0:
             if nums1[m] >= nums2[n]:
-                nums1[idx_new] = nums1[m]
-                m-=1
+                nums1[mn] = nums1[m]
+                m -= 1
             else:
-                nums1[idx_new] = nums2[n]
-                n-=1
-            idx_new-=1
+                nums1[mn] = nums2[n]
+                n -= 1
+            mn-=1
         
-        while m>=0:
-            nums1[idx_new] = nums1[m]
-            m-=1
-            idx_new-=1
-        while n>=0:
-            nums1[idx_new] = nums2[n]
+        while n >= 0:
+            nums1[mn] = nums2[n]
             n-=1
-            idx_new-=1
-        return n
-            
-                
-        
-        
-        
-        
-        
-        
+            mn-=1
+        return nums1
