559d88baaf8f92111ab26bf76075557ce08a29cd
saviaga <saviaga@gmail.com>
2022-09-08 15:15:59 -0400

    Time: 2152 ms (13.95%), Space: 23.7 MB (75.99%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -5,51 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        def partition(start, end):
-            #To get better performance on sorted or nearly sorted data we randomize 
-            #by random sampling of one of the array elements as the pivot.
-            #we swap the selected item with the last element
-            
-            ran = random.randint(start, end)
-            pivot = end
-            nums[pivot], nums[ran] = nums[ran], nums[pivot]
-     
-            # Index of smaller element
-            border = start
-            for cur in range(start, end):
-                if nums[cur] >= nums[pivot]:
-                    nums[cur], nums[border] = nums[border], nums[cur]
-                    border += 1
-            
-            nums[border], nums[pivot] = nums[pivot], nums[border]
-            return border
+        ordered = []
+        for elem in nums:
+            heapq.heappush(ordered,elem)
+            if len(ordered)>k:
+                 heapq.heappop(ordered)
 
-        def quickselect(start, end, k_largest):
-            """
-            Returns the k-th smallest element of list within left..right
-            """
-            
-            while start<=end:
+        return ordered[0]
                 
-
-                # get pivot index  
-                pivot_index = partition(start,end)
-
-                # the pivot is in its final sorted position
-                if k_largest == pivot_index:
-                     return nums[k_largest]
-                # go left
-                elif  pivot_index < k_largest:
-                    start = pivot_index+1
-                # go right
-                else:
-                    end = pivot_index-1
-
-            # kth largest is (n - k)th smallest 
-            return nums[k_largest]
-        
-        return  quickselect(0, len(nums)-1, k-1)
-
         
         
         
