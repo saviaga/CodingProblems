099784a81bdd0aaf2ce3b0beefec5e79110d624e
saviaga <saviaga@gmail.com>
2022-09-07 21:26:35 -0400

    Time: 70 ms (26.11%), Space: 13.9 MB (38.30%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,21 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        mn=m+n-1
-        m=m-1
-        n=n-1
+        p3=m+n-1
+        p1=m-1
+        p2=n-1
         
-        while m >= 0 and n >= 0:
-            if nums1[m] >= nums2[n]:
-                nums1[mn] = nums1[m]
-                m -= 1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] >= nums2[p2]:
+                nums1[p3] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[mn] = nums2[n]
-                n -= 1
-            mn-=1
+                nums1[p3] = nums2[p2]
+                p2 -= 1
+            p3-=1
         
-        while n >= 0:
-            nums1[mn] = nums2[n]
-            n-=1
-            mn-=1
+        while p2 >= 0:
+            nums1[p3] = nums2[p2]
+            p2-=1
+            p3-=1
         return nums1
