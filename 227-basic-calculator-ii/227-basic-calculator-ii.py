aa57bc425d042ae254e6fad01e7f75cc20f55706
saviaga <saviaga@gmail.com>
2022-09-08 10:55:16 -0400

    Time: 266 ms (8.57%), Space: 15.1 MB (96.66%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,19 +1,26 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        i=0
-        cur, prev, res = 0, 0, 0
-        cur_operator =  '+'
         
-        while i<len(s):
-           
+        cur_operator = '+'
+        res = 0
+        cur = 0
+        prev = 0
+        i=0
+        
+        #grab the first number
+        while i < len(s):
             
+            #process the first number which can contain several digits #324+24
             if s[i].isdigit():
-                while i<len(s) and s[i].isdigit():
-                    cur=cur*10+int(s[i])
+                while i <len(s) and s[i].isdigit():
+                    #0 *10 + 3  = 3
+                    #3 *10 + 2  = 32
+                    #32*10 + 4  = 324
+                    cur = cur* 10 + int(s[i])
                     i+=1
+                ##324+24
+                #    ^  is gonna finish there so we need to go back one place to grab the operator at the last iteration. 
                 i-=1
-               
-           
                 if cur_operator == '+':
                     res += cur
                     prev = cur
@@ -33,6 +40,4 @@ class Solution:
                 cur_operator = s[i]
             i+=1
         return res            
-            
-                
         
