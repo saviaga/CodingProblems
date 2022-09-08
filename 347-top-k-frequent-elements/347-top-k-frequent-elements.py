c4411aadd9cbfabb2ea5231758a0f75e4fc049b4
saviaga <saviaga@gmail.com>
2022-09-08 18:33:40 -0400

    Time: 266 ms (6.98%), Space: 18.8 MB (48.01%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,17 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        occurrences = collections.Counter(nums)
-        flat_list = []
-        bucket = [[] for _ in range(len(nums) + 1)]
-        for num, freq in occurrences.items():
-            bucket[freq].append(num) 
 
-        for sublist in bucket:
-            for elem in sublist:
-                flat_list.append(elem)
- 
-        return flat_list[::-1][:k]
-    
-    
+        k=k-1
+        def quickselect(l,r,nums):
+            #
             
+            pivot = nums[r]
+            p = l
+            for i in range(l,r):
+                
+                if nums[i][1] > pivot[1]:
+                    nums[p], nums[i] = nums[i],nums[p]
+                    p+=1
+            nums[p],nums[r]= nums[r],nums[p]
+            if p > k: return quickselect(l,p-1,nums)
+            elif p < k: return quickselect(p+1,r,nums)
+            else:   
+                return [elem[0] for elem in nums[:k+1]]
+        
+        items = {}
+        for elem in nums:
+            items[elem] = items.setdefault(elem,0)+1
+        
+            
+        
+        return quickselect(0,len(items)-1,list(items.items()))
+        
