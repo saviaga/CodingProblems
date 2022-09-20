49328bf9a7c37fdede61ff87076f4451b09ba035
saviaga <saviaga@gmail.com>
2022-09-20 16:23:02 -0400

    Time: 33 ms (33.37%), Space: 13.3 MB (89.35%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,19 +1,21 @@
 class Solution:
     def myPow(self, x, n):
-        if n<0:
-            return 1/self.myPowHelp(x,abs(n))
-        else:
-            return self.myPowHelp(x,n)
 
-    def myPowHelp(self,x,n):
-            if n==0:return 1
-            if x==0: return 0
-            
-            temp = self.myPowHelp(x,n//2)
-            result = temp*temp
-            if n%2==1:
-                result= result*x
-            return result
+        def myPowHelp(x,n):
+                if n==0:return 1
+                if x==0: return 0
+
+                temp = myPowHelp(x,n//2)
+                result = temp*temp
+                if n%2==1:
+                    result= result*x
+                return result
+        
+        if n<0:
+            return 1/myPowHelp(x,abs(n))
+        else:
+            return myPowHelp(x,n)
+
            
       
         
