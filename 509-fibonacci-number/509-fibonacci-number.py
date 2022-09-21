bbd8f5fb5c0a35af187ba6f13cff63fb6dedb972
saviaga <saviaga@gmail.com>
2022-09-20 22:05:14 -0400

    Time: 2510 ms (5.02%), Space: 13.4 MB (34.01%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        fibo = {0:0,1:1}
+        if n<2:
+            return n
+        else:
+           
+            return self.fib(n-1)+self.fib(n-2)
+        
