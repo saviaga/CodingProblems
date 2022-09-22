ae839cbd6c1854487b4ff83235c2ae18bea936fa
saviaga <saviaga@gmail.com>
2022-09-22 19:32:32 -0400

    Time: 49 ms (89.48%), Space: 14.5 MB (11.95%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -49,9 +49,10 @@ class Solution(object):
         start = 0
         for end in range(len(s)):
                 
-            while s[end] in chars:
-                    chars.remove(s[start])
-                    start+=1
+            if s[end] in chars:
+                while s[end] in chars:
+                        chars.remove(s[start])
+                        start+=1
             chars.add(s[end])
             longest= max(longest,len(chars))
             
