518677de85d35d08db6782659afd4a6aa84f0e81
saviaga <saviaga@gmail.com>
2022-09-23 18:16:19 -0400

    Time: 88 ms (42.29%), Space: 14.1 MB (80.45%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,28 +1,21 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start = 0
+        end = len(s)-1
         
-        
-        #pointer start, end
-        #first check if pointer start is alpha, if it is not, move it forward
-        #first check if pointer end is alpha, if it is not, move it back
-        #if both are alpha, then first convert to lower and compare
-        #if they are different then return false
-        #keep comparing until start<=end
-        
-        start=0
-        end=len(s)-1
-        
-        while start<=end:
+        while start <= end:
             if not s[start].isalnum():
                 start+=1
                 continue
-            elif not s[end].isalnum():
+            if not s[end].isalnum():
                 end-=1
                 continue
-            #condition 1
-            elif s[start].lower() != s[end].lower():
-                    return False
-            
+            if s[start].lower() !=s[end].lower():
+                return False
             start+=1
             end-=1
         return True
