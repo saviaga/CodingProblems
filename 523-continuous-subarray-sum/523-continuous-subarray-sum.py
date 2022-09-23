216bcc3c6433940bdc273a644fd05bbf9a8c34fc
saviaga <saviaga@gmail.com>
2022-09-23 13:12:30 -0400

    Time: 2426 ms (5.10%), Space: 33.3 MB (40.23%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -1,22 +1,22 @@
-class Solution:
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        
-        dic_rem = {0:-1}
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
         curr_sum = 0
-        
-        
+        freq = {0:-1}
         for i in range(len(nums)):
             curr_sum+=nums[i]
-            
             remainder = curr_sum%k
-
-            if remainder not in dic_rem:
-                dic_rem[remainder]=i
-            elif i - dic_rem[remainder] > 1:
-                   
-                    return True 
+          
+            if remainder not in freq:
+                freq[remainder]=i
+            elif i - freq[remainder]>1:
+                return True
             
-        return False
                 
+        return False
             
         
