d2f501d5fe6036dc1eeb249f35164ba83dc35674
saviaga <saviaga@gmail.com>
2022-09-24 19:33:10 -0400

    Time: 2022 ms (15.61%), Space: 33.5 MB (19.15%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -5,18 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        curr_sum = 0
+        cum_sum = 0
         freq = {0:-1}
-        for i in range(len(nums)):
-            curr_sum+=nums[i]
-            remainder = curr_sum%k
-          
-            if remainder not in freq:
-
-                freq[remainder]=i
-            elif i - freq[remainder]>1:
-                return True
-                
-        return False
+        for i  in range(len(nums)):
+            cum_sum+=nums[i]
+            remainder = cum_sum%k
             
+            if remainder not in freq:
+                freq[remainder] = i
+            elif i-freq[remainder]>1:
+                return True
+        return False
+  
         
