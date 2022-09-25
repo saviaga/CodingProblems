22448c52bfb9fffcfd1d78712fea9af4856a9c66
saviaga <saviaga@gmail.com>
2022-09-25 15:58:53 -0400

    Time: 83 ms (22.57%), Space: 13.6 MB (30.79%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -1,5 +1,4 @@
 class Solution(object):
-        
     def intersect(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -7,21 +6,27 @@ class Solution(object):
         :rtype: List[int]
         """
         
+        #Approach 1:
+        #store nums1 in a map and count ocurrences
+        #loop trhough nums2 and check if is in map and ocurrences>0:
+        #if yes, store in res and rest ocurrences
+        #Time O(N+M), we need to loop through both arrays
+        #Space O(m) number of distinct characters in nums1 + space of res
         
+        #Approach2: Use Two pointers and check coincidences(do not care about duplicates)
+        #sort arrays
+        #Store in res
+        #Time O(nlogn)
+        #Space O(1) constant with variables + space of res
         
-        if len(nums1) > len(nums2):
-            return self.intersect(nums2,nums1)
-        else:
-            res = []
-            dic_nums1 = Counter(nums1)
-            for elem in nums2:
-                if elem in dic_nums1:
-                    dic_nums1[elem] -=1
-                    res.append(elem)
-                    if dic_nums1[elem]==0:
-                        del dic_nums1[elem]
-            return res
-
+        mapping = Counter(nums1)
+        res = []
+        for elem in nums2:
+            if elem in mapping and mapping[elem]>0:
+                res.append(elem)
+                mapping[elem]-=1
+        return res
+                
                 
                 
         
