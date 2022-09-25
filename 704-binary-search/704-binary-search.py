7ca76b0a4212d93747c941ebbf6f6123a39d7154
saviaga <saviaga@gmail.com>
2022-09-24 23:44:43 -0400

    Time: 560 ms (5.05%), Space: 14.6 MB (77.42%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -5,18 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
         start = 0
         end = len(nums)-1
         
-        while start<= end:
-            mid = (start+end)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[mid]<target:
+        while start <= end:
+            mid = (start+end) // 2
+            if nums[mid] > target:
+                end = mid-1
+            elif nums[mid] < target:
                 start = mid+1
-            else:
-                end = mid -1
+            elif nums[mid]==target:
+                return mid
+
         return -1
-            
-                
+        
