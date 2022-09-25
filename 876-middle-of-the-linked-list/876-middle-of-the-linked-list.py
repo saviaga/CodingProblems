28ab87fc9cb6fb94b2e9b1fd8db9c445401125d5
saviaga <saviaga@gmail.com>
2022-09-25 13:37:18 -0400

    Time: 19 ms (85.84%), Space: 13.6 MB (26.16%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -9,11 +9,11 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        slow = head
         fast = head
         while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-        
-        return slow
+            fast = fast.next.next            
+            head = head.next
+
+        return head
+            
         
