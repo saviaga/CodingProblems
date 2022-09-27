6cad4c76e64ffd067d4fd9f6e666f48955bdb7ff
saviaga <saviaga@gmail.com>
2022-09-27 16:58:11 -0400

    Time: 34 ms (28.50%), Space: 13.5 MB (37.08%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -11,10 +11,10 @@ class Solution(object):
             
         inner_list = []
         past_row = self.generate(numRows-1)
-        
-        for j in range(0,len(past_row[-1])-1):
+        last_list = past_row[-1]
+        for j in range(0,len(last_list)-1):
                 
-                inner_list.append(past_row[-1][j]+past_row[-1][j+1])
+                inner_list.append(last_list[j]+last_list[j+1])
 
         full_row = [1] + inner_list + [1]
         past_row.append(full_row) 
