76528d40821638d93998463a3f3ca1f7d07bc7f8
saviaga <saviaga@gmail.com>
2022-09-27 10:38:50 -0400

    Time: 34 ms (19.76%), Space: 13.5 MB (12.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -4,10 +4,21 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        def climbStairsHelper(i,n,dp):
+            if i>n:
+                return 0
+            if i==n:
+                return 1
+            
+            
+            if dp[i] > 0:
+                return dp[i]
+            dp[i] = climbStairsHelper(i+1,n,dp) + climbStairsHelper(i+2,n,dp)
+            return dp[i]
+            
+            
+            
+            
         dp = [0]*(n+1)
-        dp[0] = 1
-        dp[1] = 1 
-        
-        for i in range(2,n+1):
-            dp[i] = dp[i-1]+dp[i-2]
-        return dp[n]
+        return climbStairsHelper(0,n,dp)
+       
