020b87722abf4756c70fadbf61d926e20b5d3be1
saviaga <saviaga@gmail.com>
2022-09-27 16:48:01 -0400

    Time: 28 ms (53.31%), Space: 13.4 MB (37.08%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -4,22 +4,24 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        def generatehelp(i):
+        def generatehelp(past_row, i):
           
             if i==numRows:
                 return 
             
             inner_list = []
-            past_row= self.dp[-1]
-            for j in range(0,len(past_row)-1):                
+            for j in range(0,len(past_row)-1):
+                
                 inner_list.append(past_row[j]+past_row[j+1])
+
             full_row = [1] + inner_list + [1]
+          
             self.dp.append(full_row)
-            return generatehelp(i+1)
+            return generatehelp(full_row,i+1)
         
         
         self.dp= [[1]]
-        generatehelp(1)
+        generatehelp([1],1)
         return self.dp
         
         
