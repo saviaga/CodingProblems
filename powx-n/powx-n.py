2dfbf5bee5b48a148c1784cc14d1c666df194683
saviaga <saviaga@gmail.com>
2022-09-28 16:05:34 -0400

    Time: 32 ms, Memory: 13.2 MB - LeetHub

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -1,22 +1,21 @@
 class Solution:
     def myPow(self, x, n):
 
-        def myPowHelp(x,n):
-                if n==0:return 1
-
-                temp = myPowHelp(x,n//2)
-                
-                if n%2==0:
-                    return temp*temp
-                else:
-                    return temp*temp*x
-        
-        if n<0:
-            return 1/myPowHelp(x,abs(n))
-        else:
-            return myPowHelp(x,n)
-
+        if n< 0:
+            x = 1/x
+            n = abs(n)
+            
+        res = 1 
+        while n>0:
+            if n%2==1:
+                res = res*x
+            x=x*x
+            n=n//2
            
+        
+        return res
+        
+       
       
         
 
