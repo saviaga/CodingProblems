b00e269e7029e0ac12ca07e3404b7a70d3989457
saviaga <saviaga@gmail.com>
2022-09-30 16:39:22 -0400

    Time: 104 ms, Memory: 20.7 MB - LeetHub

---
+++ linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        #U: There is no null, hence that is a cycle
+        # if we arrive again to the same element there is a cyle
+        #If we find a null there is no cycle
+        #if empty there is a cycle
+        
+        #M: Fast and Low Pointer
+        #if they at some point meet, there is a cycle
+        #if at some point we reach null with either pointer there isn't a cycle
+        
+        #P: While both pointers are different continue moving pointers, or while both pointers are different of null
+        # if pointers are equal return true
+        #if any pointer is null return false
+        
+        if not head: 
+            return False
+        
+        slow = head
+        fast = head.next
+        
+        while slow!=fast:
+            
+            if fast==None or fast.next==None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+            
+        return True
+        
+        
+        
+        
