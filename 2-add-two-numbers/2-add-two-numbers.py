4c484feb87f633261d7d091c3497eb4ee46d2f33
saviaga <saviaga@gmail.com>
2022-10-01 12:17:28 -0400

    Time: 121 ms (20.85%), Space: 13.5 MB (68.33%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -33,31 +33,17 @@ class Solution(object):
         
         
         
-        def addTwoNumbersHelper(node, v1,v2,carry_val):
-            #print(node.val,v1,v2,carry_val)
-            if not v1 and not v2 and not carry_val:
-                return
-            
-            
-            val1 = v1.val if v1 else 0
-            val2 = v2.val if v2 else 0
-            curr_suma = val1 + val2 + carry_val
-
-            node_val = curr_suma%10
-            carry_val = curr_suma//10
-            node.next = ListNode(node_val)
-            v1 = v1.next if v1 else None
-            v2 = v2.next if v2 else None
-            node =node.next
-
-            return addTwoNumbersHelper(node,v1,v2,carry_val)
-        
-        
-        sum_node = ListNode()
-        new_head = sum_node
-        addTwoNumbersHelper(sum_node,l1,l2,0)
-        return new_head.next
-            
+    def addTwoNumbers(self, l1, l2):
+        curr_sum = l1.val + l2.val
+        node_val=curr_sum % 10
+        carry = curr_sum // 10
+        node = ListNode(node_val)
+        if any((l1.next, l2.next, carry)):
+            l1 = l1.next if l1.next else ListNode(0)
+            l2 = l2.next if l2.next else ListNode(0)
+            l1.val += carry
+            node.next = self.addTwoNumbers(l1, l2)    
+        return node            
         
                 
             
