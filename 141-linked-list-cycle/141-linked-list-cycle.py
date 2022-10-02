804cc1f5186a07837210d464ec3f5b8f8c04f5e0
saviaga <saviaga@gmail.com>
2022-10-02 11:57:27 -0400

    Time: 64 ms (66.47%), Space: 20.5 MB (40.14%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -10,34 +10,17 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        #U: There is no null, hence that is a cycle
-        # if we arrive again to the same element there is a cyle
-        #If we find a null there is no cycle
-        #if empty there is a cycle
         
-        #M: Fast and Low Pointer
-        #if they at some point meet, there is a cycle
-        #if at some point we reach null with either pointer there isn't a cycle
-        
-        #P: While both pointers are different continue moving pointers, or while both pointers are different of null
-        # if pointers are equal return true
-        #if any pointer is null return false
-        
-        if not head: 
-            return False
+        if not head:
+            return
         
         slow = head
         fast = head.next
         
         while slow!=fast:
-            if fast==None or fast.next==None:
+            if fast == None or fast.next == None:
                 return False
             slow = slow.next
             fast = fast.next.next
         return True
             
-            
-        
-        
-        
-        
