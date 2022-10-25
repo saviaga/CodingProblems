85901b6c69d993b2b95280304606b451ab4b8309
saviaga <saviaga@gmail.com>
2022-10-25 10:08:42 -0400

    Time: 62 ms (87.70%), Space: 13.4 MB (98.74%) - LeetHub

---
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def lengthOfLongestSubstringKDistinct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        
+        #s = "eceba" k=2
+        dict_ch = {e:1,
+                   b:1
+                    }
+        longest = 3 
+        """
+        start = 0
+        end = 0
+        
+        dict_ch = {}
+        longestsub = 0
+        
+        while end < len(s):
+            
+            #the char needs to be counted
+            if s[end] in dict_ch:
+                dict_ch[s[end]]+=1
+            else:
+                dict_ch[s[end]] = 1
+            #check if the number of keys > k:
+            
+            while len(dict_ch)>k:
+                dict_ch[s[start]]-=1
+                if dict_ch[s[start]] == 0:
+                    del dict_ch[s[start]]
+                start+=1
+            #if this is true then reduce the size of window
+            longestsub = max(longestsub,end-start+1)
+            #else: check the current size, and check if it is larger
+            end+=1
+            
+        return longestsub 
+            
+        
+                
+            
+        
